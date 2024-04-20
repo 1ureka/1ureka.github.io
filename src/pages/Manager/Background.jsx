e99e0c4b1d2c7602d2d00f6542df965a399228fc
1ureka <summerheadquarters001@gmail.com>
2024-04-20 19:59:48 +0800

    lint, refactor, optmize

--- src/pages/Manager/Background.jsx
+++ src/pages/Manager/Background.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from "react";
-import { useTheme } from "@mui/material/styles";
 import { useRecoilValue } from "recoil";
 import { useRecoilValueLoadable } from "recoil";
 
@@ -27,30 +26,40 @@ function ImageBox({ name }) {
   );
 }
 
-function getBoxes() {
-  const rows = useRecoilValue(ALBUM_ROWS);
-  const random = gsap.utils.shuffle(rows.map((item) => item.name));
-  const names = random.slice(0, 4);
-
-  const heights = [1.5, 1, 2.75, 1.75, 2, 1];
-  const config = heights.map((value) => {
-    return {
-      height: `${value * 10}%`,
-      width: "100%",
-      py: 1,
-    };
+function Wrapper({ children }) {
+  const sx = (theme) => ({
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    boxShadow: theme.shadows[3],
+    borderRadius: "5px",
   });
 
-  const elements = [
-    <ImageBox name={names[0]} />,
-    <FillBox color="primary" />,
-    <ImageBox name={names[1]} />,
-    <ImageBox name={names[2]} />,
-    <ImageBox name={names[3]} />,
-    <FillBox color="secondary" />,
+  return <Box sx={sx}>{children}</Box>;
+}
+
+function Boxes({ names }) {
+  const heights = [1.5, 1, 2.75, 1.75, 2, 1];
+  const styles = heights.map((value) => ({
+    height: `${value * 10}%`,
+    width: "100%",
+    py: 1,
+  }));
+
+  const configs = [
+    { sx: styles[0], e: <ImageBox name={names[0]} /> },
+    { sx: styles[1], e: <FillBox color="primary" /> },
+    { sx: styles[2], e: <ImageBox name={names[1]} /> },
+    { sx: styles[3], e: <ImageBox name={names[2]} /> },
+    { sx: styles[4], e: <ImageBox name={names[3]} /> },
+    { sx: styles[1], e: <FillBox color="secondary" /> },
   ];
 
-  return [config, elements];
+  return configs.map(({ sx, e }, i) => (
+    <Box key={i} sx={sx}>
+      <Wrapper>{e}</Wrapper>
+    </Box>
+  ));
 }
 
 function useAnimation(ref1, ref2) {
@@ -63,16 +72,8 @@ function useAnimation(ref1, ref2) {
 }
 
 export default function Background() {
-  const [config, elements] = getBoxes();
-
-  const theme = useTheme();
-  const wrapperSx = {
-    position: "relative",
-    width: "100%",
-    height: "100%",
-    boxShadow: theme.shadows[3],
-    borderRadius: "5px",
-  };
+  const rows = useRecoilValue(ALBUM_ROWS);
+  const names = gsap.utils.shuffle(rows.map((item) => item.name));
 
   const ref1 = useRef(null);
   const ref2 = useRef(null);
@@ -81,19 +82,10 @@ export default function Background() {
   return (
     <>
       <Box sx={{ height: "100%" }} ref={ref1}>
-        {config.map((sx, i) => (
-          <Box key={i} sx={sx}>
-            <Box sx={wrapperSx}>{elements[i]}</Box>
-          </Box>
-        ))}
+        <Boxes names={names} />
       </Box>
-
       <Box sx={{ height: "100%" }} ref={ref2}>
-        {config.map((sx, i) => (
-          <Box key={i} sx={sx}>
-            <Box sx={wrapperSx}>{elements[i]}</Box>
-          </Box>
-        ))}
+        <Boxes names={names} />
       </Box>
     </>
   );
