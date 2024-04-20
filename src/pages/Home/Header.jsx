37b97c8a49a9bdffd8dc1f93cdeed4b569c0deb3
1ureka <summerheadquarters001@gmail.com>
2024-04-20 15:37:19 +0800

    full responsive Header

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
+
 import { Toolbar, AppBar, Divider, useMediaQuery } from "@mui/material";
-import { Container, Stack, Avatar } from "@mui/material";
+import { Container, Stack, Avatar, Drawer } from "@mui/material";
 import { Typography, Tooltip, IconButton } from "@mui/material";
 import { Tabs, Tab } from "@mui/material";
 
 import Inventory2Icon from "@mui/icons-material/Inventory2";
 import LoginRoundedIcon from "@mui/icons-material/LoginRounded";
 import MenuOpenRoundedIcon from "@mui/icons-material/MenuOpenRounded";
-import ThemeControl from "../../components/ThemeControl";
+import ThemeControl, { ThemeToggles } from "../../components/ThemeControl";
 
 import { useRecoilValue } from "recoil";
 import { HOME_IS_AUTH, HOME_PAGE } from "../../utils/store";
@@ -14,6 +16,7 @@ import { useNavigateTo } from "../../utils/hooks";
 import { useScrollTo } from "./useScrollTo";
 
 const profileImage = "/favicon.png";
+const list = ["Intro", "Scene", "Props"];
 
 function Title({ sx }) {
   const homePage = useRecoilValue(HOME_PAGE);
@@ -62,7 +65,6 @@ function Navigation() {
     "&:hover": { backgroundColor: "divider" },
   };
 
-  const list = ["Intro", "Scene", "Props"];
   const value = list[homePage];
   const scrollTo = useScrollTo();
   const handleChange = (_, val) => {
@@ -82,16 +84,17 @@ function Navigation() {
   );
 }
 
-function Tools({ sx }) {
+function Tools() {
   const isAuth = useRecoilValue(HOME_IS_AUTH);
   const navigate1 = useNavigateTo("/manager");
   const navigate2 = useNavigateTo("/login");
+  const match = useMediaQuery("(min-width:850px)");
 
   const homePage = useRecoilValue(HOME_PAGE);
   const isTop = homePage === 0;
 
   return (
-    <Stack sx={{ flexGrow: 0, ...sx }} direction="row" spacing={0.5}>
+    <Stack sx={{ flexGrow: 0 }} direction="row" spacing={0.5}>
       {isAuth ? (
         <Tooltip title="Manage Files">
           <IconButton onClick={navigate1} color={isTop ? "inherit" : "default"}>
@@ -105,54 +108,90 @@ function Tools({ sx }) {
           </IconButton>
         </Tooltip>
       )}
-      <ThemeControl color={isTop ? "inherit" : "default"} />
+      {match ? <ThemeControl color={isTop ? "inherit" : "default"} /> : ""}
     </Stack>
   );
 }
 
+function Sidebar({ open, onClose }) {
+  const homePage = useRecoilValue(HOME_PAGE);
+  const value = list[homePage];
+  const scrollTo = useScrollTo();
+  const handleChange = (_, val) => {
+    scrollTo((current) => {
+      const target = list.indexOf(val);
+      const direction = target - current > 0 ? 1 : -1;
+      return { target, direction };
+    });
+  };
+
+  const stackProps = {
+    justifyContent: "space-between",
+  };
+
+  return (
+    <Drawer open={open} onClose={onClose}>
+      <Stack height={"100%"} sx={{ p: 1, py: 2.5 }} {...stackProps}>
+        <Tabs orientation="vertical" value={value} onChange={handleChange}>
+          <Tab label="Intro" value={"Intro"} sx={{ p: 2 }} />
+          <Tab label="Scene" value={"Scene"} sx={{ p: 2 }} />
+          <Tab label="Props" value={"Props"} sx={{ p: 2 }} />
+        </Tabs>
+        <ThemeToggles />
+      </Stack>
+    </Drawer>
+  );
+}
+
 export default function Header() {
-  const matche1 = useMediaQuery("(min-width:1050px)");
-  const matche2 = useMediaQuery("(min-width:850px)");
+  const match1 = useMediaQuery("(min-width:1050px)");
+  const match2 = useMediaQuery("(min-width:850px)");
 
   const homePage = useRecoilValue(HOME_PAGE);
   const isTop = homePage === 0;
 
+  const [open, setOpen] = useState(false);
+
+  const contents = (
+    <Toolbar>
+      <Title />
+      <Divider orientation="vertical" flexItem sx={{ mx: 1.5 }}></Divider>
+      <Navigation />
+      <Tools />
+    </Toolbar>
+  );
+
+  const mobilContents = (
+    <Toolbar>
+      <Tooltip title="Menu">
+        <IconButton
+          onClick={() => setOpen(true)}
+          color={isTop ? "inherit" : "default"}
+        >
+          <MenuOpenRoundedIcon />
+        </IconButton>
+      </Tooltip>
+      <Title sx={{ flexGrow: 1, justifyContent: "center" }} />
+      <Tools />
+      <Sidebar open={open} onClose={() => setOpen(false)} />
+    </Toolbar>
+  );
+
+  const addBarSx = {
+    color: isTop ? "white" : "inherit",
+    py: 1.5,
+    px: match1 ? 12 : 0,
+    transition: "all 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+    backdropFilter: isTop ? "blur(0px)" : " blur(10px)",
+    backgroundColor: isTop ? "appBarHide" : "appBar",
+    boxShadow: isTop
+      ? "0px 0px 1px -1px rgba(0,0,0,0),0px 1px 1px 0px rgba(0,0,0,0),0px 1px 3px 0px rgba(0,0,0,0)"
+      : "0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)",
+  };
+
   return (
-    <AppBar
-      position="absolute"
-      sx={{
-        color: isTop ? "white" : "inherit",
-        py: 1.5,
-        px: matche1 ? 12 : 0,
-        transition: "all 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-        backdropFilter: isTop ? "blur(0px)" : " blur(10px)",
-        backgroundColor: isTop ? "appBarHide" : "appBar",
-        boxShadow: isTop
-          ? "0px 0px 1px -1px rgba(0,0,0,0),0px 1px 1px 0px rgba(0,0,0,0),0px 1px 3px 0px rgba(0,0,0,0)"
-          : "0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)",
-      }}
-      elevation={0}
-    >
-      <Container maxWidth="xl">
-        {matche2 ? (
-          <Toolbar>
-            <Title />
-            <Divider orientation="vertical" flexItem sx={{ mx: 1.5 }}></Divider>
-            <Navigation />
-            <Tools />
-          </Toolbar>
-        ) : (
-          <Toolbar>
-            <Tooltip title="Navigate">
-              <IconButton>
-                <MenuOpenRoundedIcon fontSize="large" />
-              </IconButton>
-            </Tooltip>
-            <Title sx={{ flexGrow: 1, justifyContent: "center" }} />
-            <Tools sx={{ flexDirection: "column" }} />
-          </Toolbar>
-        )}
-      </Container>
+    <AppBar position="absolute" sx={addBarSx} elevation={0}>
+      <Container maxWidth="xl">{match2 ? contents : mobilContents}</Container>
     </AppBar>
   );
 }
