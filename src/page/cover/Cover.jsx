7c8be7256879cec29b1028f038f289ec47f490d0
1ureka <summerheadquarters001@gmail.com>
2024-04-30 13:44:13 +0800

    continue making Cover

--- src/page/cover/Cover.jsx
+++ src/page/cover/Cover.jsx
@@ -1,18 +1,56 @@
-import { Box, Stack } from "@mui/material";
+import * as React from "react";
+import { Box, Stack, ThemeProvider, Tooltip } from "@mui/material";
+import { Radio, RadioGroup } from "@mui/material";
+
 import Layout from "../../components/layout/Layout";
+import { darkTheme } from "../../utils/theme";
 
 function Background() {
-  const imgStyle = { width: "100%", height: "100%", objectFit: "cover" };
+  const imgStyle = {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    scale: "1.05",
+  };
+
+  const gradientSx = {
+    ...imgStyle,
+    background: "radial-gradient(transparent, transparent, rgb(0 0 0 / 0.35))",
+  };
   return (
     <Box sx={{ position: "absolute", inset: 0 }}>
-      <img
-        src="https://fakeimg.pl/1920x1080/?text=こんにちは&font=noto"
-        alt=""
-        style={imgStyle}
-      />
+      <img src="./PJ28-2 とびら-1.webp" style={imgStyle} alt="" />
+      <Box sx={gradientSx} />
     </Box>
   );
 }
+
+function Title(params) {}
+
+function Radios() {
+  const [val, setVal] = React.useState(0);
+
+  const sizeSx = { "& .MuiSvgIcon-root": { fontSize: 22 } };
+
+  return (
+    <Tooltip title="Change Cover" placement="left">
+      <RadioGroup value={val} onChange={({ target }) => setVal(target.value)}>
+        {[0, 1, 2].map((i) => (
+          <Radio key={i} value={i} sx={sizeSx} />
+        ))}
+      </RadioGroup>
+    </Tooltip>
+  );
+}
+
+// change according auth
+function MainButton(params) {}
+
+// Cover name
+function SubTitle(params) {}
+
+function Control(params) {}
 
 function ContentLayout({ justify, align, children }) {
   return (
@@ -31,7 +69,9 @@ function Content() {
   return (
     <Stack sx={{ height: "100%" }}>
       <ContentLayout justify="center" align="flex-start"></ContentLayout>
-      <ContentLayout justify="flex-end" align="center"></ContentLayout>
+      <ContentLayout justify="flex-end" align="center">
+        <Radios />
+      </ContentLayout>
       <ContentLayout justify="center" align="flex-end"></ContentLayout>
     </Stack>
   );
@@ -50,8 +90,10 @@ export default function Cover() {
 
   return (
     <Layout exit={exit}>
-      <Background />
-      <Content />
+      <ThemeProvider theme={darkTheme}>
+        <Background />
+        <Content />
+      </ThemeProvider>
     </Layout>
   );
 }
