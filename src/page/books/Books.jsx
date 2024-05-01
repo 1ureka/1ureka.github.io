c7c4e4ddf189aaf1e776cf7febe6975b13f4037d
1ureka <summerheadquarters001@gmail.com>
2024-05-01 19:49:57 +0800

    u

--- src/page/books/Books.jsx
+++ src/page/books/Books.jsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { Box, Divider, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  ButtonBase,
+  Divider,
+  Skeleton,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { motion } from "framer-motion";
 
-import { BOOKS_TAB } from "../../utils/store";
+import { BOOKS_ROWS, BOOKS_TAB, THEME } from "../../utils/store";
 import Tabs from "../../components/generic/Tabs";
 import Layout from "../../components/generic/Layout";
 
@@ -66,8 +73,93 @@ function ContentIntroTypo({ info }) {
   );
 }
 
+function FakeImg() {
+  return (
+    <Skeleton
+      variant="rounded"
+      sx={{ width: "100%", height: "100%" }}
+      animation="wave"
+    />
+  );
+}
+
+function ImageReflect({ hover, x, clipPath }) {
+  const theme = useRecoilValue(THEME);
+
+  const reflectVariants = {
+    notHover: { opacity: 0, x: x - 60 },
+    hover: { opacity: 1, x },
+  };
+
+  const background = `linear-gradient(150deg, ${theme.palette.divider}, transparent)`;
+
+  return (
+    <MotionStack
+      variants={reflectVariants}
+      animate={hover ? "hover" : "notHover"}
+      sx={{
+        zIndex: 1,
+        pointerEvents: "none",
+        position: "absolute",
+        inset: 0,
+        clipPath,
+        background,
+        filter: "blur(35px)",
+      }}
+    ></MotionStack>
+  );
+}
+
+function AlbumImage() {
+  const [hover, setHover] = React.useState(false);
+  return (
+    <MotionStack
+      variants={itemVariants}
+      whileTap={{ scale: 0.97 }}
+      whileHover={{ scale: 1.02, filter: "brightness(1.05)", rotate: 2 }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      sx={{ position: "relative", overflow: "hidden" }}
+    >
+      <ButtonBase sx={{ width: "100%", aspectRatio: "16/9" }}>
+        <FakeImg />
+      </ButtonBase>
+      <ImageReflect
+        hover={hover}
+        x={-30}
+        clipPath={"polygon(35% 0, 50% 0, 25% 100%, 10% 100%)"}
+      />
+      <ImageReflect
+        hover={hover}
+        x={10}
+        clipPath={"polygon(40% 0, 50% 0, 25% 100%, 15% 100%)"}
+      />
+    </MotionStack>
+  );
+}
+
+function Album() {
+  const rows = useRecoilValue(BOOKS_ROWS);
+
+  const containerSx = {
+    position: "relative",
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(240px, 1fr))",
+    gap: "14px",
+  };
+
+  return (
+    <Box sx={containerSx}>
+      {rows.map(({ name }) => (
+        <AlbumImage key={name} />
+      ))}
+    </Box>
+  );
+}
+
 function Content() {
   const tab = useRecoilValue(BOOKS_TAB);
+  const rows = useRecoilValue(BOOKS_ROWS);
 
   const containerSx = (theme) => ({
     backgroundColor: theme.palette.custom.content,
@@ -75,8 +167,18 @@ function Content() {
     borderRadius: "0 50px 5px 5px",
   });
 
+  const contentSx = {
+    flexGrow: 1,
+    p: 3,
+    height: "1px",
+    overflowY: "auto",
+    scrollbarGutter: "stable",
+  };
+
   const transition = { staggerChildren: 0.05, delayChildren: 0.15 };
   const variants = { animate: { transition } };
+  const albumTransition = { staggerChildren: 0.3 / rows.length };
+  const albumVariants = { animate: { transition: albumTransition } };
 
   return (
     <MotionStack sx={containerSx} variants={variants} key={intro[tab]?.project}>
@@ -87,7 +189,11 @@ function Content() {
         <ContentIntroTypo info={intro[tab]?.info} />
       </Stack>
       <Divider flexItem variant="middle" />
-      <Box sx={{ flexGrow: 1 }}></Box>
+      <Box sx={contentSx}>
+        <MotionStack variants={albumVariants}>
+          <Album />
+        </MotionStack>
+      </Box>
     </MotionStack>
   );
 }
