6fd46ac5864a9e5fbe7f378fdd9d66e21a762fca
1ureka <summerheadquarters001@gmail.com>
2024-05-05 19:38:29 +0800

    u

--- src/components/books/carousels/Slides.jsx
+++ src/components/books/carousels/Slides.jsx
@@ -1,11 +1,41 @@
+import { useRecoilValue } from "recoil";
 import { MotionStack } from "../../Motion";
+import { BOOKS_ROWS, BOOKS_SELECTED } from "../../../utils/store";
+import SlidesImage from "../image/SlidesImage";
+import { Stack } from "@mui/material";
+
+function Item({ category, name, selected }) {
+  const sx = {
+    opacity: selected ? 1 : 0.85,
+    scale: selected ? "0.9" : "0.65",
+    translate: "0 -50%",
+    transformOrigin: "right",
+  };
+  return <SlidesImage category={category} name={name} sx={sx} />;
+}
 
 export default function Slides() {
-  // maxWidth: 12.5%
-  // selected scale: 1, un scale: 0.6
+  const rows = useRecoilValue(BOOKS_ROWS);
+  const selected = useRecoilValue(BOOKS_SELECTED);
+
   return (
     <MotionStack
-      sx={{ position: "absolute", right: 0, width: "12.5%", height: "100%" }}
-    ></MotionStack>
+      sx={{
+        position: "absolute",
+        right: "1%",
+        top: 0,
+        width: "12.5%",
+        translate: "0 calc(50vh + (-100% / 15) * 0)",
+      }}
+    >
+      {rows.map(({ category, name }, i) => (
+        <Item
+          key={name}
+          category={category}
+          name={name}
+          selected={i === selected}
+        />
+      ))}
+    </MotionStack>
   );
 }
