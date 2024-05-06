40753b0339916e1ef66df7e5e590d0177ddf493a
1ureka <summerheadquarters001@gmail.com>
2024-05-06 12:28:55 +0800

    u

--- src/components/books/carousels/Backdrop.jsx
+++ src/components/books/carousels/Backdrop.jsx
@@ -1,33 +1,39 @@
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { motion } from "framer-motion";
-import { BOOKS_OPEN, THEME } from "../../../utils/store";
+import { BOOKS_OPEN, BOOKS_ROWS, BOOKS_SELECTED } from "../../../utils/store";
+import { THEME } from "../../../utils/store";
 
 export default function Backdrop({ children }) {
   const setOpen = useSetRecoilState(BOOKS_OPEN);
+  const setSelected = useSetRecoilState(BOOKS_SELECTED);
+  const rows = useRecoilValue(BOOKS_ROWS);
   const theme = useRecoilValue(THEME);
+
+  const variants = {
+    initial: { opacity: 0 },
+    animate: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0,
+        duration: 0.5,
+        delayChildren: 0.2,
+        staggerChildren: 0.1,
+      },
+    },
+    exit: {
+      opacity: 0,
+      transition: {
+        type: "spring",
+        bounce: 0,
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
     <motion.div
-      variants={{
-        initial: { opacity: 0 },
-        animate: {
-          opacity: 1,
-          transition: {
-            type: "spring",
-            bounce: 0,
-            duration: 0.5,
-            delayChildren: 0.2,
-            staggerChildren: 0.1,
-          },
-        },
-        exit: {
-          opacity: 0,
-          transition: {
-            type: "spring",
-            bounce: 0,
-            duration: 0.5,
-          },
-        },
-      }}
+      variants={variants}
       initial="initial"
       animate="animate"
       exit="exit"
@@ -40,7 +46,14 @@ export default function Backdrop({ children }) {
         position: "absolute",
         inset: 0,
       }}
-      onClick={() => setOpen(false)}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        setOpen(false);
+      }}
+      onWheel={(e) => {
+        const change = e.deltaY > 0 ? 1 : -1;
+        setSelected((prev) => (prev + change + rows.length) % rows.length);
+      }}
     >
       {children}
     </motion.div>
