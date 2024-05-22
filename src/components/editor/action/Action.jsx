bf26ce76042f1b8f6a628d0339d8c54194c2c0b4
1ureka <summerheadquarters001@gmail.com>
2024-05-23 06:39:59 +0800

    output function

--- src/components/editor/action/Action.jsx
+++ src/components/editor/action/Action.jsx
@@ -2,49 +2,69 @@ import * as React from "react";
 import { Button, CircularProgress } from "@mui/material";
 import { useRecoilState, useRecoilValue } from "recoil";
 
-import { EDITOR_FILTER, EDITOR_INPUT } from "../../../utils/store";
+import { EDITOR_DISPLAY, EDITOR_FILTER } from "../../../utils/store";
 import { EDITOR_OUTPUT_SETTING } from "../../../utils/store";
-import { EDITOR_SELECTED } from "../../../utils/store";
+import { EDITOR_SELECTED, EDITOR_INPUT } from "../../../utils/store";
+import { compressImage, filterImage } from "../../../utils/utils";
+import { blobGetDataUrl } from "../../../utils/utils";
 
-function useAction() {
-  const [selected, setSelected] = useRecoilState(EDITOR_SELECTED);
-  const [input, setInput] = useRecoilState(EDITOR_INPUT);
+function useOptions() {
   const outputOptions = useRecoilValue(EDITOR_OUTPUT_SETTING);
   const filterOptions = useRecoilValue(EDITOR_FILTER);
+  const options = { ...outputOptions, ...filterOptions };
+  options.maxSize *= 1024 * 1024;
+
+  return options;
+}
+
+function processImages({ input, selected, options }) {
+  return Promise.all(
+    selected.map(async (name) => {
+      const file = input.find((file) => file.name === name);
+      const filtered = await filterImage(file, options);
+      const blob = await compressImage(filtered, options);
+      const dataUrl = await blobGetDataUrl(blob);
+
+      return {
+        dataUrl,
+        name: name.slice(0, name.lastIndexOf(".")),
+      };
+    })
+  );
+}
+
+function downloadImages(results) {
+  results.forEach(({ dataUrl, name }) => {
+    const link = document.createElement("a");
+    link.href = dataUrl;
+    link.download = name;
+    link.click();
+  });
+}
+
+function useAction() {
+  const [input, setInput] = useRecoilState(EDITOR_INPUT);
+  const [selected, setSelected] = useRecoilState(EDITOR_SELECTED);
+  const [display, setDisplay] = useRecoilState(EDITOR_DISPLAY);
 
   const [loading, setLoading] = React.useState(false);
-  const findFile = (name) => input.find((file) => file.name === name);
+  const disabled = selected.length === 0 || loading;
+  const options = useOptions();
 
   const action = async () => {
     setLoading(true);
+    const results = await processImages({ input, selected, options });
 
-    /** @type {string[]} */
-    const results = await Promise.all(
-      selected.map((name) => {
-        const file = findFile(name);
-        console.log(`compress ${file.name}... with options`);
-        console.table(outputOptions);
-        console.table(filterOptions);
-      })
-    );
-
-    setInput((prev) =>
-      prev.filter((file) => !selected.some((name) => file.name === name))
-    );
+    const isSelected = (name) => selected.includes(name);
+    if (isSelected(display)) setDisplay("");
+    setInput((prev) => prev.filter((file) => !isSelected(file.name)));
     setSelected([]);
 
-    // const list = results.map((result, i) => ({ name: names[i], ...result }));
-    // list.forEach(({ dataUrl, name }) => {
-    //   const link = document.createElement("a");
-    //   link.href = dataUrl;
-    //   link.download = `${name.substring(0, name.lastIndexOf("."))}.${type}`;
-    //   link.click();
-    // });
-
+    downloadImages(results);
     setLoading(false);
   };
 
-  return [action, loading, selected.length === 0 || loading];
+  return [action, loading, disabled];
 }
 
 export default function Action() {
