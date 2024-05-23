ffc7f059373a1f4ae25395e05c16bd798bf482bb
1ureka <summerheadquarters001@gmail.com>
2024-05-23 20:05:01 +0800

    remake hooks

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,14 +1,39 @@
 import { useEffect, useRef, useState } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { useRecoilValueLoadable } from "recoil";
 
-import { MANAGER_ADDED, MANAGER_CATEGORY, MANAGER_DELED } from "./store";
-import { INDEX, TABLE_SELECTED, IMAGES } from "./store";
-import { deleteFile, uploadFile, loadFile } from "./utils";
-import { decode, delay, blobGetDataUrl } from "./utils";
+import {
+  useRecoilValue,
+  useSetRecoilState,
+  useRecoilValueLoadable,
+  useRecoilState,
+} from "recoil";
+
+import {
+  BOOKS_OPEN,
+  BOOKS_ROWS,
+  BOOKS_SELECTED,
+  EDITOR_INPUT,
+  IMAGES,
+  INDEX,
+  MANAGER_ADDED,
+  MANAGER_CATEGORY,
+  MANAGER_DELED,
+  TABLE_PAGE,
+  TABLE_SELECTED,
+} from "./store";
+
+import {
+  deleteFile,
+  uploadFile,
+  loadFile,
+  decode,
+  delay,
+  blobGetDataUrl,
+  compressImage,
+  filterImage,
+} from "./utils";
 
 //
-// communicate
+// Global
 export function useSyncIndex() {
   const setIndex = useSetRecoilState(INDEX);
 
@@ -27,63 +52,6 @@ export function useSyncIndex() {
   return syncIndex;
 }
 
-export function useImageAdd() {
-  const syncIndex = useSyncIndex();
-  const setSelected = useSetRecoilState(TABLE_SELECTED);
-  const setAdded = useSetRecoilState(MANAGER_ADDED);
-  const category = useRecoilValue(MANAGER_CATEGORY);
-
-  const uploadImages = async (list) => {
-    await Promise.all(
-      list.map(({ name: n, thumbnail: t, origin: o }) =>
-        Promise.all([
-          uploadFile(t.split(",")[1], `images/${category}/${n}/1K/${n}.webp`),
-          uploadFile(o.split(",")[1], `images/${category}/${n}/4K/${n}.webp`),
-        ])
-      )
-    );
-  };
-
-  /** @param {Object[]} list @param {string} list[].name @param {string} list[].thumbnail @param {string} list[].origin */
-  return async (list) => {
-    await uploadImages(list);
-    setSelected((prev) => {
-      const set = new Set([...prev, ...list.map((val) => val.name)]);
-      return Array.from(set);
-    });
-    await syncIndex();
-    setAdded(list.length);
-  };
-}
-
-export function useImageDelete() {
-  const syncIndex = useSyncIndex();
-  const setSelected = useSetRecoilState(TABLE_SELECTED);
-  const setDeled = useSetRecoilState(MANAGER_DELED);
-  const category = useRecoilValue(MANAGER_CATEGORY);
-
-  const deleteImages = async (names = [""]) => {
-    await Promise.all(
-      names.map((n) =>
-        Promise.all([
-          deleteFile(`images/${category}/${n}/1K/${n}.webp`),
-          deleteFile(`images/${category}/${n}/4K/${n}.webp`),
-        ])
-      )
-    );
-  };
-
-  /**@param {string[]} names */
-  return async (names) => {
-    await deleteImages(names);
-    setSelected([]);
-    await syncIndex();
-    setDeled(names.length);
-  };
-}
-
-//
-// image loading and decoding
 export function useBlob(blob) {
   const [state, setState] = useState(false);
   const [src, setSrc] = useState("");
@@ -133,7 +101,75 @@ export function useDecode(src) {
   return [_src, state];
 }
 
-export function useImageLoad(category, name, size) {
+export function useDropArea(onDrop) {
+  const [dragOver, setDragOver] = React.useState(false);
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "copy";
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDragOver(false);
+    onDrop(e.dataTransfer.files);
+  };
+
+  const DropProps = {
+    onDragEnter: () => setDragOver(true),
+    onDragLeave: () => setDragOver(false),
+    onDrop: handleDrop,
+    onDragOver: handleDragOver,
+  };
+
+  return [dragOver, DropProps];
+}
+
+//
+// Books
+export function useBooksGroups() {
+  const rows = useRecoilValue(BOOKS_ROWS);
+
+  const groups = {};
+  rows.forEach((item, i) => {
+    const matches = item.name.match(`^(.+)-\\d+`);
+    const groupName = matches ? matches[1] : item.name;
+    if (!groups[groupName]) groups[groupName] = [];
+    groups[groupName].push({ ...item, i });
+  });
+
+  return { rows, groups };
+}
+
+export function useBooksGroupClick() {
+  const [group, setGroup] = useState("");
+
+  const createHandlerG = (group) => async () => {
+    await delay(200);
+    setGroup(group);
+  };
+
+  return { group, createHandlerG };
+}
+
+export function useBooksImageClick() {
+  const setOpen = useSetRecoilState(BOOKS_OPEN);
+  const setSelected = useSetRecoilState(BOOKS_SELECTED);
+
+  useEffect(() => {
+    return () => setOpen(false);
+  }, [setOpen]);
+
+  const createHandlerI = (index) => async () => {
+    await delay(200);
+    setSelected(index);
+    setOpen(true);
+  };
+
+  return { createHandlerI };
+}
+
+export function useBooksImageLoad(category, name, size) {
   const [state, setState] = useState(false);
   const [src, setSrc] = useState("");
   const dataUrl = useRecoilValueLoadable(
@@ -152,7 +188,7 @@ export function useImageLoad(category, name, size) {
   return [src, state];
 }
 
-export function useImageDecode(category, name, size) {
+export function useBooksImageDecode(category, name, size) {
   const [state, setState] = useState(false);
   const [src, setSrc] = useState("");
   const timeStamp = useRef(false);
@@ -181,4 +217,210 @@ export function useImageDecode(category, name, size) {
   }, [dataUrl.state, dataUrl.contents]);
 
   return [src, state];
+}
+
+//
+// Manager
+export function useManagerSelect() {
+  const [task, setTask] = useState("");
+  const [hint, setHint] = useState("");
+  const [list, setList] = useState([]);
+
+  const createInput = () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
+    input.multiple = true;
+    input.style.display = "none";
+    return input;
+  };
+
+  const getInput = async () => {
+    const input = createInput();
+    const fileList = await new Promise((resolve) => {
+      input.addEventListener("cancel", () => resolve([]));
+      input.addEventListener("change", () => resolve(input.files));
+      document.body.appendChild(input);
+      input.click();
+    });
+    input.remove();
+    return Array.from(fileList);
+  };
+
+  /** @param {File[]} files*/
+  const processImages = (files) => {
+    return Promise.all(
+      files.map(async (file) => {
+        const blobO = await compressImage(file);
+        const blobT = await compressImage(file, { scale: 0.125 });
+        const origin = await blobGetDataUrl(blobO);
+        const thumbnail = await blobGetDataUrl(blobT);
+        const name = file.name.replace(/\.[^.]+$/, "");
+        return { name, origin, thumbnail };
+      })
+    );
+  };
+
+  const action = async () => {
+    setHint("");
+    setTask("Selecting files...");
+
+    const files = await getInput();
+    if (files.length === 0) {
+      setHint("No file selected");
+      setTask("");
+      return;
+    }
+    if (!files.every((file) => file.type.match("image.*"))) {
+      setHint("Please select only image files");
+      setTask("");
+      return;
+    }
+
+    setTask("Compressing files...");
+    const list = await processImages(files);
+    setTask("");
+    setList(list);
+  };
+
+  return { action, task, hint, list };
+}
+
+export function useManagerUpload() {
+  const syncIndex = useSyncIndex();
+  const setSelected = useSetRecoilState(TABLE_SELECTED);
+  const setAdded = useSetRecoilState(MANAGER_ADDED);
+  const category = useRecoilValue(MANAGER_CATEGORY);
+
+  const uploadImages = async (list) => {
+    await Promise.all(
+      list.map(({ name: n, thumbnail: t, origin: o }) =>
+        Promise.all([
+          uploadFile(t.split(",")[1], `images/${category}/${n}/1K/${n}.webp`),
+          uploadFile(o.split(",")[1], `images/${category}/${n}/4K/${n}.webp`),
+        ])
+      )
+    );
+  };
+
+  /** @param {Object[]} list @param {string} list[].name @param {string} list[].thumbnail @param {string} list[].origin */
+  return async (list) => {
+    await uploadImages(list);
+    setSelected((prev) => {
+      const set = new Set([...prev, ...list.map((val) => val.name)]);
+      return Array.from(set);
+    });
+    await syncIndex();
+    setAdded(list.length);
+  };
+}
+
+export function useManagerDelete() {
+  const syncIndex = useSyncIndex();
+  const setSelected = useSetRecoilState(TABLE_SELECTED);
+  const setDeled = useSetRecoilState(MANAGER_DELED);
+  const setPage = useSetRecoilState(TABLE_PAGE);
+  const category = useRecoilValue(MANAGER_CATEGORY);
+
+  const deleteImages = async (names = [""]) => {
+    await Promise.all(
+      names.map((n) =>
+        Promise.all([
+          deleteFile(`images/${category}/${n}/1K/${n}.webp`),
+          deleteFile(`images/${category}/${n}/4K/${n}.webp`),
+        ])
+      )
+    );
+  };
+
+  /**@param {string[]} names */
+  return async (names) => {
+    await deleteImages(names);
+    setSelected([]);
+    setPage(0);
+    await syncIndex();
+    setDeled(names.length);
+  };
+}
+
+//
+// Editor
+export function useEditorInput() {
+  const [input, setInput] = useRecoilState(EDITOR_INPUT);
+  const inputNames = input.map((item) => item.file.name);
+
+  const copyName = (fileName) => {
+    const dotIndex = fileName.lastIndexOf(".");
+    const name = fileName.slice(0, dotIndex);
+    const extension = fileName.slice(dotIndex + 1);
+    return `${name} (copy).${extension}`;
+  };
+
+  return (action = (fileList) => {
+    const files = Array.from(fileList);
+    const imageFiles = files.filter((file) => file.type.startsWith("image/"));
+
+    const newFiles = imageFiles.map((file) => {
+      if (!inputNames.includes(file.name)) return file;
+      return new File([file], copyName(file.name), { type: file.type });
+    });
+
+    if (newFiles.length === 0) return;
+
+    const newInput = newFiles.map((file, i) => ({
+      selected: true,
+      display: i + 1 === newFiles.length,
+      file,
+    }));
+
+    setInput((prev) => {
+      const prevInput = prev.map((item) => ({ ...item, display: false }));
+      return [...prevInput, ...newInput];
+    });
+  });
+}
+
+export function useEditorOutput() {
+  const [input, setInput] = useRecoilState(EDITOR_INPUT);
+  const selected = input.filter((item) => item.selected);
+
+  const [loading, setLoading] = React.useState(false);
+  const disabled = selected.length === 0 || loading;
+
+  const outputOptions = useRecoilValue(EDITOR_OUTPUT);
+  const filterOptions = useRecoilValue(EDITOR_FILTER);
+  const options = { ...outputOptions, ...filterOptions };
+  options.maxSize *= 1024 * 1024;
+
+  const processImages = () => {
+    return Promise.all(
+      selected.map(async ({ file }) => {
+        const filtered = await filterImage(file, options);
+        const blob = await compressImage(filtered, options);
+        const dataUrl = await blobGetDataUrl(blob);
+
+        return {
+          dataUrl,
+          name: file.name.slice(0, file.name.lastIndexOf(".")),
+        };
+      })
+    );
+  };
+
+  const action = async () => {
+    setLoading(true);
+    const results = await processImages();
+    setInput((prev) => prev.filter((item) => !item.selected));
+
+    results.forEach(({ dataUrl, name }) => {
+      const link = document.createElement("a");
+      link.href = dataUrl;
+      link.download = name;
+      link.click();
+    });
+
+    setLoading(false);
+  };
+
+  return [action, loading, disabled];
 }
