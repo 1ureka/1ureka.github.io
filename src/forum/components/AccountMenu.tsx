238e2e4a8e815664e084ce3739b2d4a38cb98fa0
1ureka <summerheadquarters001@gmail.com>
2025-03-18 21:46:47 +0800

    更新 AccountMenu 組件，新增 slotsProps 參數以支持自定義 Popover 屬性

--- src/forum/components/AccountMenu.tsx
+++ src/forum/components/AccountMenu.tsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import { Avatar, Button, Divider, IconButton, ListItemIcon, MenuItem, MenuList, Popover } from "@mui/material";
+import type { PopoverProps } from "@mui/material";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
 import ManageAccountsRoundedIcon from "@mui/icons-material/ManageAccountsRounded";
 import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 
-const AccountMenu = ({ user, mobile }: { user: string; mobile: boolean }) => {
+const AccountMenu = ({
+  user,
+  mobile,
+  slotsProps,
+}: {
+  user: string;
+  mobile?: boolean;
+  slotsProps?: { popover?: Partial<PopoverProps> };
+}) => {
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => setAnchorEl(event.currentTarget);
@@ -15,7 +24,9 @@ const AccountMenu = ({ user, mobile }: { user: string; mobile: boolean }) => {
     <>
       {mobile ? (
         <IconButton onClick={handleClick} edge="end" size="small">
-          <Avatar sx={{ bgcolor: "primary.main" }}>{user.slice(0, 1).toUpperCase()}</Avatar>
+          <Avatar sx={{ bgcolor: "primary.main", width: "2rem", height: "2rem" }}>
+            {user.slice(0, 1).toUpperCase()}
+          </Avatar>
         </IconButton>
       ) : (
         <Button variant="outlined" color="inherit" startIcon={<AccountCircleRoundedIcon />} onClick={handleClick}>
@@ -28,6 +39,7 @@ const AccountMenu = ({ user, mobile }: { user: string; mobile: boolean }) => {
         anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
         open={open}
         onClose={onClose}
+        {...slotsProps?.popover}
       >
         <MenuList dense>
           <MenuItem onClick={onClose}>
