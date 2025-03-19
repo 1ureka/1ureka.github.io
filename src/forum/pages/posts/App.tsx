0ff722041e21cd7d67f731a71896a264c95b19bc
1ureka <summerheadquarters001@gmail.com>
2025-03-20 00:44:17 +0800

    新增 OrderTabs 組件並整合至 App，顯示文章排序功能，並使用 ExpandedPost 顯示文章詳細資訊

--- src/forum/pages/posts/App.tsx
+++ src/forum/pages/posts/App.tsx
@@ -1,12 +1,16 @@
-import { Box, Button, Container, CssBaseline, Paper, ThemeProvider } from "@mui/material";
-import ArrowRightAltRoundedIcon from "@mui/icons-material/ArrowRightAltRounded";
+import { Box, Container, CssBaseline, Divider, Paper, Stack } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 
+import { posts } from "@/forum/utils/test";
 import "@/forum/app.css";
+
 import { Toaster } from "@/forum/components/Toast";
 import { AppbarDesktop } from "@/forum/components/AppbarDesktop";
 import { AppbarMobile } from "@/forum/components/AppbarMobile";
 import { ScrollArea } from "@/forum/components/ScrollArea";
 import { theme, useResponsiveFontSize } from "@/forum/utils/theme";
+import { ExpandedPost } from "@/forum/components/ExpandedPost";
+import { OrderTabs } from "@/forum/components/OrderTabs";
 
 function App() {
   const { isMd } = useResponsiveFontSize();
@@ -23,11 +27,18 @@ function App() {
 
         <Container maxWidth="lg" sx={{ position: "relative", my: 10 }}>
           <Paper sx={{ py: 3, borderRadius: 3, border: "1px solid", borderColor: "divider" }} elevation={1}>
-            <Box sx={{ mx: 1.5 }}>
-              <Button variant="outlined" color="primary" fullWidth endIcon={<ArrowRightAltRoundedIcon />}>
-                查看更多
-              </Button>
+            <Box sx={{ position: "relative", px: { xs: 1, sm: 2, md: 4 } }}>
+              <Box sx={{ position: "absolute", inset: 0, bgcolor: "divider", opacity: 0.35 }} />
+              <OrderTabs />
             </Box>
+
+            <Divider />
+
+            <Stack sx={{ alignItems: "stretch", mb: 1.5 }}>
+              {posts.map((post) => (
+                <ExpandedPost key={post.id} post={post} />
+              ))}
+            </Stack>
           </Paper>
         </Container>
       </ScrollArea>
