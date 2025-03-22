087d75e9205a4142a10684e4497da4564af486c6
1ureka <summerheadquarters001@gmail.com>
2025-03-22 11:13:56 +0800

    新增假資料模擬 API，實作多個獲取評論的 Hook

--- src/forum/hooks/comment.ts
+++ src/forum/hooks/comment.ts
@@ -1,1 +1,73 @@
-// 根據 post id 獲取 commentId array (單層), 根據 comment id 獲取 comment, 根據 comment id 獲取 linked comment id array (以此類推)
+// ----------------------------------------
+// 假資料與模擬 API
+// ----------------------------------------
+
+import { useQuery } from "@tanstack/react-query";
+import { comments } from "../utils/data";
+
+const fakeFetchCommentsByPostId = async (postId: number) => {
+  await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+  return (
+    comments
+      .filter((comment) => {
+        const isMainComment = comment.postId === postId && !comment.parentId;
+        return isMainComment;
+      })
+      .map((comment) => comment.id) || []
+  );
+};
+
+const fakeFetchCommentsByParentId = async (parentId: number) => {
+  await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+  return comments.filter((comment) => comment.parentId === parentId).map((comment) => comment.id) || [];
+};
+
+const fakeFetchCommentsByCommentId = async (commentId: number) => {
+  await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+  return comments.filter((comment) => comment.parentId === commentId).map((comment) => comment.id) || [];
+};
+
+const fakeFetchCommentById = async (commentId: number) => {
+  await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
+  return comments.find((comment) => comment.id === commentId) || null;
+};
+
+// ----------------------------------------
+// 實際 Hook
+// ----------------------------------------
+
+const staleTime = 1000 * 60 * 10;
+
+const useCommentsByPostId = (postId: number) => {
+  return useQuery({
+    queryKey: ["commentsByPost", postId],
+    queryFn: () => fakeFetchCommentsByPostId(postId),
+    staleTime,
+  });
+};
+
+const useCommentsByParentId = (parentId: number) => {
+  return useQuery({
+    queryKey: ["commentsByParent", parentId],
+    queryFn: () => fakeFetchCommentsByParentId(parentId),
+    staleTime,
+  });
+};
+
+const useCommentsByCommentId = (commentId: number) => {
+  return useQuery({
+    queryKey: ["commentsByComment", commentId],
+    queryFn: () => fakeFetchCommentsByCommentId(commentId),
+    staleTime,
+  });
+};
+
+const useCommentById = (commentId: number) => {
+  return useQuery({
+    queryKey: ["comment", commentId],
+    queryFn: () => fakeFetchCommentById(commentId),
+    staleTime,
+  });
+};
+
+export { useCommentsByPostId, useCommentsByParentId, useCommentsByCommentId, useCommentById };
