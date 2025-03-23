f630de240df69f5e644b83a5d0241236cafa6540
1ureka <summerheadquarters001@gmail.com>
2025-03-23 14:59:59 +0800

    新增 ThemeDrawer 組件以支援手機板主題切換功能，並整合至 ThemeMenu

--- src/forum/components/ThemeMenu.tsx
+++ src/forum/components/ThemeMenu.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import { IconButton, MenuItem, MenuList, Popover, Tooltip, useColorScheme } from "@mui/material";
-import type { PopoverProps } from "@mui/material";
+import { Popover, Tooltip, Typography, useColorScheme } from "@mui/material";
+import { Box, Drawer, IconButton, MenuItem, MenuList } from "@mui/material";
+import type { DrawerProps, PopoverProps } from "@mui/material";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
 
 const ThemeMenuWithButton = () => {
@@ -52,4 +54,36 @@ const ThemeMenu = ({ onClose, ...props }: PopoverProps & { onClose: () => void }
   );
 };
 
-export { ThemeMenuWithButton, ThemeMenu };
+const ThemeDrawer = ({ onClose, ...props }: DrawerProps & { onClose: () => void }) => {
+  const { mode, setMode } = useColorScheme();
+
+  const createHandler = (mode: "light" | "dark" | "system") => () => {
+    setMode(mode);
+    onClose();
+  };
+
+  return (
+    <Drawer anchor="top" onClose={onClose} {...props}>
+      <Box sx={{ p: 3, display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+        <Typography variant="h6">切換主題</Typography>
+        <IconButton onClick={onClose}>
+          <CloseRoundedIcon />
+        </IconButton>
+      </Box>
+
+      <MenuList dense>
+        <MenuItem onClick={createHandler("light")} selected={mode === "light"}>
+          淺色
+        </MenuItem>
+        <MenuItem onClick={createHandler("dark")} selected={mode === "dark"}>
+          暗色
+        </MenuItem>
+        <MenuItem onClick={createHandler("system")} selected={mode === "system"}>
+          跟隨系統
+        </MenuItem>
+      </MenuList>
+    </Drawer>
+  );
+};
+
+export { ThemeMenuWithButton, ThemeMenu, ThemeDrawer };
