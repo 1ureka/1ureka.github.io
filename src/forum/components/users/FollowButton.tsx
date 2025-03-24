a75f54b7f5e3b3c44af3dc2d03520cde2c5e19d2
1ureka <summerheadquarters001@gmail.com>
2025-03-24 21:03:38 +0800

    新增 FollowButton 元件，實現使用者追蹤和編輯個人資料的功能

---
+++ src/forum/components/users/FollowButton.tsx
@@ -0,0 +1,73 @@
+import { Button } from "@mui/material";
+import NotificationAddRoundedIcon from "@mui/icons-material/NotificationAddRounded";
+import EditNoteRoundedIcon from "@mui/icons-material/EditNoteRounded";
+import { useUser } from "@/forum/hooks/user";
+import { useSession } from "@/forum/hooks/session";
+
+const FollowButton = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const { data: user, isFetching } = useUser(urlParams.get("user"));
+  const { user: userSession, loading: isLoadingSession } = useSession();
+
+  if (!isFetching && user === null) {
+    window.location.replace("/404");
+    return null;
+  }
+
+  if (isLoadingSession || isFetching || !user || !userSession) {
+    return (
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        startIcon={<NotificationAddRoundedIcon />}
+        loading
+        sx={{ borderRadius: 99, transition: "all 0.2s ease", boxShadow: 3, scale: "1.001" }}
+      >
+        追蹤
+      </Button>
+    );
+  }
+
+  if (userSession.name === user.name) {
+    return (
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        startIcon={<EditNoteRoundedIcon />}
+        sx={{
+          borderRadius: 99,
+          transition: "all 0.2s ease",
+          boxShadow: 3,
+          scale: "1.001",
+          "&:hover": { bgcolor: "primary.main", scale: "1.05", boxShadow: 3 },
+          "&:active": { scale: "0.95" },
+        }}
+      >
+        編輯個人資料
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      size="large"
+      startIcon={<NotificationAddRoundedIcon />}
+      sx={{
+        borderRadius: 99,
+        transition: "all 0.2s ease",
+        boxShadow: 3,
+        scale: "1.001",
+        "&:hover": { bgcolor: "primary.main", scale: "1.05", boxShadow: 3 },
+        "&:active": { scale: "0.95" },
+      }}
+    >
+      追蹤
+    </Button>
+  );
+};
+
+export { FollowButton };
