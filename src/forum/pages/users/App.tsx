ef0e5f3b38545855d97a7d3e74efb9133187a8ab
1ureka <summerheadquarters001@gmail.com>
2025-03-24 20:45:18 +0800

    重構使用者頁面，新增使用者資料加載邏輯，改善使用者介面顯示

--- src/forum/pages/users/App.tsx
+++ src/forum/pages/users/App.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Container, Divider, Paper, Typography } from "@mui/material";
+import { Avatar, Box, Button, CircularProgress, Container, Divider, Paper, Skeleton, Typography } from "@mui/material";
 import PeopleRoundedIcon from "@mui/icons-material/PeopleRounded";
 import NotificationAddRoundedIcon from "@mui/icons-material/NotificationAddRounded";
 import BarChartRoundedIcon from "@mui/icons-material/BarChartRounded";
@@ -10,23 +10,146 @@ import { AppbarDesktop } from "@/forum/components/appbar/AppbarDesktop";
 import { AppbarMobile } from "@/forum/components/appbar/AppbarMobile";
 import { ScrollArea } from "@/forum/components/ScrollArea";
 import { useResponsiveFontSize } from "@/forum/utils/theme";
-import { authors } from "@/forum/utils/data";
+import { useUser } from "@/forum/hooks/user";
 
-function App() {
-  const { isMd } = useResponsiveFontSize();
+const UserAvatar = ({ size = 6, borderSize = 0.8 }: { size?: number; borderSize?: number }) => {
   const urlParams = new URLSearchParams(window.location.search);
-  const user = authors.find((author) => author.name === urlParams.get("user"));
-  if (user === undefined) {
-    window.location.replace("/");
+  const { data: user, isFetching } = useUser(urlParams.get("user"));
+
+  if (!isFetching && user === null) {
+    window.location.replace("/404");
     return null;
   }
 
-  const avatorSize = 6;
-  const borderOffset = 0.8;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        position: "relative",
+        width: `${size}rem`,
+        alignSelf: "flex-end",
+        m: 2,
+      }}
+    >
+      <Box sx={{ position: "absolute", bottom: 0, width: 1, height: "auto", aspectRatio: 1 }}>
+        <Box
+          sx={{
+            borderRadius: "50%",
+            position: "absolute",
+            inset: `${-borderSize}rem`,
+            bgcolor: "background.paper",
+            backgroundImage: "var(--Paper-overlay)",
+          }}
+        />
+      </Box>
+
+      {!isFetching && user ? (
+        <Avatar
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            width: 1,
+            height: "auto",
+            aspectRatio: 1,
+            bgcolor: "primary.main",
+            boxShadow: 3,
+          }}
+        >
+          <Typography variant="h3" component="span" sx={{ translate: "0px 5%", color: "white" }}>
+            {user.name.charAt(0).toUpperCase()}
+          </Typography>
+        </Avatar>
+      ) : (
+        <>
+          <Skeleton
+            variant="circular"
+            sx={{ position: "absolute", bottom: 0, width: 1, height: "auto", aspectRatio: 1 }}
+            animation="wave"
+          />
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: 0,
+              width: 1,
+              height: "auto",
+              aspectRatio: 1,
+              display: "grid",
+              placeItems: "center",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        </>
+      )}
+    </Box>
+  );
+};
+
+const UserTitle = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const { data: user, isFetching } = useUser(urlParams.get("user"));
+
+  if (!isFetching && user === null) {
+    window.location.replace("/404");
+    return null;
+  }
+
+  // 以使用者名稱查詢，則載入時可以先顯示，但載入完成後會被取代
+  if (urlParams.get("user") && isFetching) {
+    return <title>{`論壇樣板 | ${urlParams.get("user")}`}</title>;
+  }
+
+  // 載入完成後顯示
+  if (user) {
+    return <title>{`論壇樣板 | ${user.name}`}</title>;
+  }
+
+  return null;
+};
+
+const UserIntro = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const { data: user, isFetching } = useUser(urlParams.get("user"));
+
+  if (!isFetching && user === null) {
+    window.location.replace("/404");
+    return null;
+  }
+
+  if (isFetching || !user) {
+    return (
+      <Box sx={{ m: 2 }}>
+        <Skeleton variant="rounded" animation="wave">
+          <Typography variant="h5" component="h2">
+            使用者名稱
+          </Typography>
+        </Skeleton>
+        <Skeleton variant="text" animation="wave">
+          <Typography variant="body2">佔位符佔位符佔位符佔位符佔位符佔位符佔位符</Typography>
+        </Skeleton>
+      </Box>
+    );
+  }
+
+  return (
+    <Box sx={{ m: 2 }}>
+      <Typography variant="h5" component="h2">
+        {user.name}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {user.description}
+      </Typography>
+    </Box>
+  );
+};
+
+function App() {
+  const { isMd } = useResponsiveFontSize();
 
   return (
     <AppWrapper>
-      <title>{`論壇樣板 | ${user.name}`}</title>
+      <UserTitle />
 
       <Box sx={{ bgcolor: "secondary.main", height: "35vh", position: "absolute", inset: "0 0 auto 0" }} />
 
@@ -36,62 +159,11 @@ function App() {
         <Container maxWidth="lg" sx={{ position: "relative", my: 10 }}>
           <Paper sx={{ pb: 3, borderRadius: 3, border: "1px solid", borderColor: "divider" }} elevation={1}>
             <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                px: 3,
-                flexWrap: "wrap",
-              }}
+              sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", px: 3, flexWrap: "wrap" }}
             >
               <Box sx={{ display: "flex", alignItems: "center" }}>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    position: "relative",
-                    width: `${avatorSize}rem`,
-                    alignSelf: "flex-end",
-                    m: 2,
-                  }}
-                >
-                  <Box sx={{ position: "absolute", bottom: 0, width: 1, height: "auto", aspectRatio: 1 }}>
-                    <Box
-                      sx={{
-                        borderRadius: "50%",
-                        position: "absolute",
-                        inset: `${-borderOffset}rem`,
-                        bgcolor: "background.paper",
-                        backgroundImage: "var(--Paper-overlay)",
-                      }}
-                    />
-                  </Box>
-
-                  <Avatar
-                    sx={{
-                      position: "absolute",
-                      bottom: 0,
-                      width: 1,
-                      height: "auto",
-                      aspectRatio: 1,
-                      bgcolor: "primary.main",
-                      boxShadow: 3,
-                    }}
-                  >
-                    <Typography variant="h3" component="span" sx={{ translate: "0px 5%", color: "white" }}>
-                      {user.name.charAt(0).toUpperCase()}
-                    </Typography>
-                  </Avatar>
-                </Box>
-
-                <Box sx={{ m: 2 }}>
-                  <Typography variant="h5" component="h2">
-                    {user.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {user.description}
-                  </Typography>
-                </Box>
+                <UserAvatar />
+                <UserIntro />
               </Box>
 
               <Box sx={{ m: 2 }}>
