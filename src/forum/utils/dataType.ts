13c3c602cbe323146d81e84c75d8fb257c6926e3
1ureka <summerheadquarters001@gmail.com>
2025-03-28 00:18:17 +0800

    移除不必要的 Post 類型定義，並更新相關的匯出內容

--- src/forum/utils/dataType.ts
+++ src/forum/utils/dataType.ts
@@ -10,32 +10,6 @@ type Notification = {
   content: string;
 };
 
-type Photo = {
-  name: string;
-  url: string;
-};
-
-type Post = {
-  // 實際來自關聯查詢的 User
-  authorId: User["id"];
-  author: User["name"];
-  authorDescription: User["description"];
-
-  // 實際來自快取視圖 post_interaction_counts, post_comments_count
-  likeCount: number;
-  replyCount: number;
-
-  id: number;
-  title: string;
-  content: string;
-  tags: string[]; // 盡量超過 3 個
-  photos?: Photo[]; // 照片連結
-  attachments?: File[]; // 附件連結
-  viewCount: number;
-  createdAt: Date;
-  updatedAt: Date;
-};
-
 type Comment = {
   // 實際來自關聯查詢的 User
   authorId: User["id"];
@@ -50,4 +24,4 @@ type Comment = {
   updatedAt: Date;
 };
 
-export type { User, Notification, Post, Comment };
+export type { User, Notification, Comment };
