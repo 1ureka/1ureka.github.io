6dc23f21d080c08ccc0b316e2475f208736180b3
1ureka <summerheadquarters001@gmail.com>
2025-04-01 21:25:27 +0800

    重構 App.tsx，簡化元件導入，新增 BarChart 元件，並優化樣式設定

--- src/datahub/pages/App.tsx
+++ src/datahub/pages/App.tsx
@@ -1,21 +1,4 @@
-import {
-  Box,
-  ButtonBase,
-  Divider,
-  FormControlLabel,
-  FormHelperText,
-  Paper,
-  Stack,
-  Switch,
-  Tooltip,
-  Typography,
-} from "@mui/material";
-import type { BoxProps, IconProps, TooltipProps } from "@mui/material";
-import BackupTableRoundedIcon from "@mui/icons-material/BackupTableRounded";
-import SummarizeRoundedIcon from "@mui/icons-material/SummarizeRounded";
-import HealthAndSafetyRoundedIcon from "@mui/icons-material/HealthAndSafetyRounded";
-import OpenInNewRoundedIcon from "@mui/icons-material/OpenInNewRounded";
-import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
+import { Box, FormControlLabel, FormHelperText, Paper, Stack, Switch, Typography } from "@mui/material";
 import TypeSpecimenRoundedIcon from "@mui/icons-material/TypeSpecimenRounded";
 
 // import { routes } from "@/routes";
@@ -24,124 +7,16 @@ import { useResponsiveFontSize } from "../utils/theme";
 import { AppNotSupported } from "../components/AppError";
 import { Appbar, APPBAR_HEIGHT } from "../components/appbar/Appbar";
 import { Sidebar } from "../components/sidebar/Sidebar";
+
 import { Header } from "../components/home/Header";
 import { TileContent, TileTitle } from "../components/home/TileText";
 import { StripedBackground } from "../components/home/StripedBackground";
+import { TileTooltip } from "../components/home/TileTooltip";
+import { SmallTiles } from "../components/home/SmallTiles";
+import { BarChart } from "../components/home/charts/BarChart";
 
-const lgSpace = { xs: 4, md: 6, xl: 8 };
-const mdSpace = { xs: 3, md: 4, xl: 5 };
-const smSpace = { xs: 1, md: 2, xl: 3 };
-const noSpace = { xs: 0, md: 0, xl: 0 };
-
-const tileIconCommonSx: IconProps["sx"] = {
-  color: "background.paper",
-  bgcolor: "primary.main",
-  borderRadius: 1,
-  p: 1,
-  fontSize: "3rem",
-};
-
-const smallTileCommonSx: BoxProps["sx"] = {
-  display: "flex",
-  alignItems: "center",
-  gap: mdSpace,
-} as const;
-
-const ellipsisSx: BoxProps["sx"] = {
-  display: "-webkit-box",
-  WebkitLineClamp: 1,
-  WebkitBoxOrient: "vertical",
-  overflow: "hidden",
-  textOverflow: "ellipsis",
-  wordBreak: "break-all",
-} as const;
-
-const underlineSx: BoxProps["sx"] = {
-  "&:hover": { textDecoration: "underline" },
-  cursor: "pointer",
-} as const;
-
-const TileTooltip = ({ children, ...props }: TooltipProps) => {
-  return (
-    <Tooltip followCursor slotProps={{ transition: { timeout: { appear: 250, enter: 250, exit: 0 } } }} {...props}>
-      {children}
-    </Tooltip>
-  );
-};
-
-const SmallTiles = () => {
-  return (
-    <Box
-      sx={{
-        position: "relative",
-        display: "grid",
-        gap: lgSpace,
-        gridTemplateColumns: { xs: "repeat(6, 1fr)", lg: "repeat(3, 1fr)" },
-      }}
-    >
-      <Box sx={{ gridColumn: { xs: "span 4", lg: "auto" }, ...smallTileCommonSx }}>
-        <BackupTableRoundedIcon sx={tileIconCommonSx} />
-
-        <Box sx={{ display: "flex", gap: smSpace }}>
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>資料庫大小</TileTitle>
-            <TileContent sx={{ textWrap: "nowrap" }}>1,234 Bytes</TileContent>
-          </Stack>
-
-          <Divider flexItem orientation="vertical" />
-
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>資料表總數</TileTitle>
-            <TileContent>8</TileContent>
-          </Stack>
-
-          <Divider flexItem orientation="vertical" />
-
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>檢視表總數</TileTitle>
-            <TileContent>6</TileContent>
-          </Stack>
-        </Box>
-      </Box>
-
-      <Box sx={{ gridColumn: { xs: "1 / span 4", lg: "auto" }, ...smallTileCommonSx }}>
-        <SummarizeRoundedIcon sx={tileIconCommonSx} />
-
-        <Box sx={{ display: "flex", gap: smSpace }}>
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>總紀錄數</TileTitle>
-            <TileContent sx={{ textWrap: "nowrap" }}>1,234</TileContent>
-          </Stack>
-
-          <Divider flexItem orientation="vertical" />
-
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>最多紀錄的資料表</TileTitle>
-            <TileTooltip title={<Typography>comments_interactions</Typography>}>
-              <TileContent sx={underlineSx}>comments_interactions</TileContent>
-            </TileTooltip>
-          </Stack>
-        </Box>
-      </Box>
-
-      <Box
-        sx={{ gridRow: { xs: "1", lg: "auto" }, gridColumn: { xs: "5 / span 2", lg: "auto" }, ...smallTileCommonSx }}
-      >
-        <HealthAndSafetyRoundedIcon sx={tileIconCommonSx} />
-
-        <Stack sx={{ alignItems: "flex-start" }}>
-          <TileTitle>潛在問題</TileTitle>
-          <TileContent sx={{ textWrap: "nowrap", color: "warning.main", display: "inline-block", ...underlineSx }}>
-            2
-            <OpenInNewRoundedIcon fontSize="small" sx={{ verticalAlign: "middle", ml: 0.5 }} />
-          </TileContent>
-        </Stack>
-      </Box>
-    </Box>
-  );
-};
-
-const recordsPercents = [81.6, 46.9, 40.8, 25.5, 5.1]; // 相對於前五筆總數的百分比 * 2
+import { lgSpace, mdSpace, smSpace } from "../components/home/commonSx";
+import { tileIconCommonSx, ellipsisSx } from "../components/home/commonSx";
 
 const LargeTiles = () => {
   return (
@@ -155,110 +30,17 @@ const LargeTiles = () => {
         width: 1,
       }}
     >
-      {/* Chart 1 */}
-      <Stack sx={{ aspectRatio: { xs: "2/1", ml: "2/1.2" }, borderTop: "1px solid", borderColor: "divider" }}>
-        <Box sx={{ display: "flex", gap: smSpace, justifyContent: "space-between", alignItems: "center", p: smSpace }}>
-          <Typography variant="h5" component="h3">
-            紀錄數
-          </Typography>
-
-          <ButtonBase sx={{ "&:hover": { bgcolor: "action.hover" }, p: 1, pr: 0, borderRadius: 1 }}>
-            <Typography sx={{ color: "text.secondary" }}>前 5 筆</Typography>
-            <ArrowDropDownRoundedIcon sx={{ color: "text.secondary" }} />
-          </ButtonBase>
-        </Box>
-
-        <Box
-          sx={{
-            position: "relative",
-            flex: 1,
-            display: "flex",
-            gap: smSpace,
-            p: smSpace,
-            py: noSpace,
-            justifyContent: "space-around",
-            alignItems: "stretch",
-          }}
-        >
-          {[...Array(5)].map((_, i) => (
-            <TileTooltip
-              key={i}
-              title={
-                <Box>
-                  <Typography variant="subtitle2">table_name_{i + 1}</Typography>
-                  <Typography>{Math.round((recordsPercents[i] * 100) / 20)} 筆紀錄</Typography>
-                </Box>
-              }
-            >
-              <Box
-                sx={{
-                  position: "relative",
-                  flex: 1,
-                  borderRadius: 2,
-                  overflow: "clip",
-                  transition: "all 0.2s ease-in-out",
-                  "&:hover": { bgcolor: "action.hover", "& .bar-content": { opacity: 1 } },
-                }}
-              >
-                <StripedBackground color1="divider" color2="#fff0" angle={-20} stripeWidth={5} />
-                <Box
-                  sx={{
-                    position: "absolute",
-                    inset: `${100 - recordsPercents[i]}% 0 0 0`,
-                    borderRadius: 2,
-                    overflow: "hidden",
-                    bgcolor: "background.paper",
-                  }}
-                >
-                  <Box
-                    className="bar-content"
-                    sx={{
-                      position: "absolute",
-                      inset: 0,
-                      bgcolor: "primary.main",
-                      opacity: i === 0 ? 0.9 : 0.65,
-                      transition: "all 0.2s ease-in-out",
-                    }}
-                  />
-                </Box>
-              </Box>
-            </TileTooltip>
-          ))}
-          <Box
-            sx={{
-              position: "absolute",
-              inset: `0 0 ${recordsPercents.reduce((a, b) => a + b, 0) / recordsPercents.length}% 0`,
-              borderBottom: "4px dashed",
-              borderColor: "divider",
-              pointerEvents: "none",
-              mx: smSpace,
-              scale: "1.02 1",
-              "&:before": {
-                content: '"平均 136"',
-                position: "absolute",
-                right: 0,
-                bottom: 0,
-                fontSize: "1rem",
-                color: "text.secondary",
-                opacity: 0.75,
-              },
-            }}
-          />
-        </Box>
-
-        <Box sx={{ display: "flex", gap: smSpace, p: smSpace, justifyContent: "space-around", alignItems: "center" }}>
-          {[...Array(5)].map((_, i) => (
-            <TileTooltip key={i} title={<Typography>table_name_{i + 1}</Typography>}>
-              <Typography variant="body1" component="p" sx={{ color: "text.secondary", ...underlineSx, ...ellipsisSx }}>
-                table_name_{i + 1}
-              </Typography>
-            </TileTooltip>
-          ))}
-        </Box>
-      </Stack>
+      <BarChart />
 
       {/* Chart 2 */}
-      <Stack sx={{ aspectRatio: { xs: "2/1", ml: "2/1.2" }, borderTop: "1px solid", borderColor: "divider" }}>
+      <Stack
+        sx={{
+          aspectRatio: { xs: "2/1", ml: "2/1.2" },
+          borderTop: "1px solid",
+          borderColor: "divider",
+          position: "relative",
+        }}
+      >
         <Box
           sx={{ display: "flex", gap: smSpace, justifyContent: "space-between", alignItems: "flex-start", p: smSpace }}
         >
@@ -269,7 +51,7 @@ const LargeTiles = () => {
             </TileContent>
           </Stack>
 
-          <TypeSpecimenRoundedIcon sx={tileIconCommonSx} />
+          <TypeSpecimenRoundedIcon sx={{ ...tileIconCommonSx, mt: 0.5 }} />
         </Box>
 
         <Box sx={{ flex: 1, position: "relative" }}>
@@ -334,10 +116,10 @@ const LargeTiles = () => {
                 <Box sx={{ py: 0.5, borderRadius: 9, bgcolor: "primary.main", filter: "hue-rotate(-10deg)" }} />
               </Box>
               <Box sx={{ width: 0.3, px: 0.5 }}>
-                <Box sx={{ py: 0.5, borderRadius: 9, bgcolor: "primary.main", filter: "hue-rotate(-30deg)" }} />
+                <Box sx={{ py: 0.5, borderRadius: 9, bgcolor: "primary.main", filter: "hue-rotate(-40deg)" }} />
               </Box>
               <Box sx={{ width: 0.1, px: 0.5, pr: 1 }}>
-                <Box sx={{ py: 0.5, borderRadius: 9, bgcolor: "primary.main", filter: "hue-rotate(-50deg)" }} />
+                <Box sx={{ py: 0.5, borderRadius: 9, bgcolor: "primary.main", filter: "hue-rotate(-70deg)" }} />
               </Box>
             </Box>
           </Box>
@@ -376,7 +158,17 @@ const LargeTiles = () => {
           </Box>
         </Box>
 
-        <Box sx={{ display: "flex", gap: mdSpace, p: smSpace, mx: smSpace, alignItems: "center" }}>
+        <Box
+          sx={{
+            display: "flex",
+            gap: mdSpace,
+            p: smSpace,
+            alignItems: "center",
+            justifyContent: "space-around",
+            width: "fit-content",
+            minWidth: 0.5,
+          }}
+        >
           {[...Array(3)].map((_, i) => (
             <Box sx={{ display: "flex", gap: 1, alignItems: "center" }} key={i}>
               <Box
@@ -385,11 +177,15 @@ const LargeTiles = () => {
                   width: "1rem",
                   height: "1rem",
                   bgcolor: "primary.main",
-                  filter: `hue-rotate(-${(i + 1) * 20}deg)`,
+                  filter: `hue-rotate(-${i * 30 + 10}deg)`,
                 }}
               />
-              <Typography variant="body1" component="p" sx={{ color: "text.secondary", ...ellipsisSx }}>
-                type_{i + 1}
+              <Typography
+                variant="body1"
+                component="p"
+                sx={{ color: "text.secondary", textTransform: "uppercase", ...ellipsisSx }}
+              >
+                {["text", "integer", "real"][i]}
               </Typography>
             </Box>
           ))}
