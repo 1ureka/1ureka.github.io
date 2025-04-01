bed3d2b9d8351b63fb7e0e85d47b6b62ddefb79e
1ureka <summerheadquarters001@gmail.com>
2025-04-01 21:24:47 +0800

    新增常用樣式設定檔，包含空間、圖示樣式及文本省略樣式

---
+++ src/datahub/components/home/commonSx.ts
@@ -0,0 +1,26 @@
+export const lgSpace = { xs: 4, md: 6, xl: 8 };
+export const mdSpace = { xs: 3, md: 4, xl: 5 };
+export const smSpace = { xs: 1, md: 2, xl: 3 };
+export const noSpace = { xs: 0, md: 0, xl: 0 };
+
+export const tileIconCommonSx = {
+  color: "background.paper",
+  bgcolor: "primary.main",
+  borderRadius: 1,
+  p: 1,
+  fontSize: "3rem",
+} as const;
+
+export const underlineSx = {
+  "&:hover": { textDecoration: "underline" },
+  cursor: "pointer",
+} as const;
+
+export const ellipsisSx = {
+  display: "-webkit-box",
+  WebkitLineClamp: 1,
+  WebkitBoxOrient: "vertical",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  wordBreak: "break-all",
+} as const;
