3b547ab28350141b0754d5bfdb4e9d187f6548b0
1ureka <summerheadquarters001@gmail.com>
2025-04-02 22:12:01 +0800

    新增資料庫大小及資料表統計顯示功能，並加入載入中狀態的骨架元件

--- src/datahub/components/home/SmallTiles.tsx
+++ src/datahub/components/home/SmallTiles.tsx
@@ -1,5 +1,5 @@
-import { Box, Divider, Stack, Typography } from "@mui/material";
-import type { BoxProps } from "@mui/material";
+import { Box, Divider, Skeleton, Stack, Typography } from "@mui/material";
+import type { BoxProps, SkeletonProps } from "@mui/material";
 import BackupTableRoundedIcon from "@mui/icons-material/BackupTableRounded";
 import SummarizeRoundedIcon from "@mui/icons-material/SummarizeRounded";
 import HealthAndSafetyRoundedIcon from "@mui/icons-material/HealthAndSafetyRounded";
@@ -8,12 +8,115 @@ import OpenInNewRoundedIcon from "@mui/icons-material/OpenInNewRounded";
 import { TileContent, TileTitle } from "./TileText";
 import { TileTooltip } from "./TileTooltip";
 import { lgSpace, mdSpace, smSpace, tileIconCommonSx, underlineSx } from "./commonSx";
+import { useDbBytes, useObjects, useRowCounts } from "@/datahub/hooks/read";
+import { formatFileSize, formatNumber } from "@/utils/formatters";
+import { toEntries } from "@/utils/typedBuiltins";
 
 const smallTileCommonSx: BoxProps["sx"] = {
   display: "flex",
   alignItems: "center",
   gap: mdSpace,
 } as const;
+
+const SkeletonWrapper = ({ children, ...props }: SkeletonProps) => (
+  <Skeleton variant="rounded" animation="wave" {...props}>
+    {children}
+  </Skeleton>
+);
+
+const Tile1 = () => {
+  const { data: size, isFetching: isFetchingSize } = useDbBytes();
+  const { data: objects, isFetching: isFetchingObjects } = useObjects({ types: ["table", "view"] });
+
+  return (
+    <Box sx={{ display: "flex", gap: smSpace }}>
+      <Stack sx={{ alignItems: "flex-start" }}>
+        <TileTitle>資料庫大小</TileTitle>
+        {size === undefined || isFetchingSize ? (
+          <SkeletonWrapper>
+            <TileContent sx={{ textWrap: "nowrap" }}>載入中 Bytes</TileContent>
+          </SkeletonWrapper>
+        ) : (
+          <TileContent sx={{ textWrap: "nowrap" }}>{formatFileSize(size)}</TileContent>
+        )}
+      </Stack>
+
+      <Divider flexItem orientation="vertical" />
+
+      <Stack sx={{ alignItems: "flex-start" }}>
+        <TileTitle>資料表總數</TileTitle>
+        {objects === undefined || isFetchingObjects ? (
+          <SkeletonWrapper>
+            <TileContent>載入中</TileContent>
+          </SkeletonWrapper>
+        ) : (
+          <TileContent sx={{ textWrap: "nowrap" }}>{objects.filter(({ type }) => type === "table").length}</TileContent>
+        )}
+      </Stack>
+
+      <Divider flexItem orientation="vertical" />
+
+      <Stack sx={{ alignItems: "flex-start" }}>
+        <TileTitle>檢視表總數</TileTitle>
+        {objects === undefined || isFetchingObjects ? (
+          <SkeletonWrapper>
+            <TileContent>載入中</TileContent>
+          </SkeletonWrapper>
+        ) : (
+          <TileContent sx={{ textWrap: "nowrap" }}>{objects.filter(({ type }) => type === "view").length}</TileContent>
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+const Tile2 = () => {
+  const { data, isFetching } = useRowCounts({ types: ["table", "view"] });
+
+  const totalCount = data ? Object.values(data).reduce((acc, count) => acc + count, 0) : 0;
+  const max = data
+    ? toEntries(data).reduce((acc, [name, count]) => {
+        if (count > acc[1]) return [name, count];
+        return acc;
+      })
+    : ["", 0];
+
+  return (
+    <Box sx={{ display: "flex", gap: smSpace }}>
+      <Stack sx={{ alignItems: "flex-start" }}>
+        <TileTitle>總紀錄數</TileTitle>
+        {data === undefined || isFetching ? (
+          <SkeletonWrapper>
+            <TileContent sx={{ textWrap: "nowrap" }}>載入中</TileContent>
+          </SkeletonWrapper>
+        ) : (
+          <TileContent sx={{ textWrap: "nowrap" }}>{formatNumber(totalCount)}</TileContent>
+        )}
+      </Stack>
+
+      <Divider flexItem orientation="vertical" />
+
+      <Stack sx={{ alignItems: "flex-start" }}>
+        <TileTitle>最多紀錄的資料表</TileTitle>
+        {data === undefined || isFetching ? (
+          <SkeletonWrapper>
+            <TileContent sx={underlineSx}>載入中</TileContent>
+          </SkeletonWrapper>
+        ) : (
+          <TileTooltip
+            title={
+              <Typography>
+                {max[0]} 有 {formatNumber(max[1] as number)} 筆紀錄
+              </Typography>
+            }
+          >
+            <TileContent sx={underlineSx}>{max[0]}</TileContent>
+          </TileTooltip>
+        )}
+      </Stack>
+    </Box>
+  );
+};
 
 const SmallTiles = () => {
   return (
@@ -27,47 +130,12 @@ const SmallTiles = () => {
     >
       <Box sx={{ gridColumn: { xs: "span 4", lg: "auto" }, ...smallTileCommonSx }}>
         <BackupTableRoundedIcon sx={tileIconCommonSx} />
-
-        <Box sx={{ display: "flex", gap: smSpace }}>
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>資料庫大小</TileTitle>
-            <TileContent sx={{ textWrap: "nowrap" }}>1,234 Bytes</TileContent>
-          </Stack>
-
-          <Divider flexItem orientation="vertical" />
-
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>資料表總數</TileTitle>
-            <TileContent>8</TileContent>
-          </Stack>
-
-          <Divider flexItem orientation="vertical" />
-
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>檢視表總數</TileTitle>
-            <TileContent>6</TileContent>
-          </Stack>
-        </Box>
+        <Tile1 />
       </Box>
 
       <Box sx={{ gridColumn: { xs: "1 / span 4", lg: "auto" }, ...smallTileCommonSx }}>
         <SummarizeRoundedIcon sx={tileIconCommonSx} />
-
-        <Box sx={{ display: "flex", gap: smSpace }}>
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>總紀錄數</TileTitle>
-            <TileContent sx={{ textWrap: "nowrap" }}>1,234</TileContent>
-          </Stack>
-
-          <Divider flexItem orientation="vertical" />
-
-          <Stack sx={{ alignItems: "flex-start" }}>
-            <TileTitle>最多紀錄的資料表</TileTitle>
-            <TileTooltip title={<Typography>comments_interactions</Typography>}>
-              <TileContent sx={underlineSx}>comments_interactions</TileContent>
-            </TileTooltip>
-          </Stack>
-        </Box>
+        <Tile2 />
       </Box>
 
       <Box
@@ -77,10 +145,16 @@ const SmallTiles = () => {
 
         <Stack sx={{ alignItems: "flex-start" }}>
           <TileTitle>潛在問題</TileTitle>
-          <TileContent sx={{ textWrap: "nowrap", color: "warning.main", display: "inline-block", ...underlineSx }}>
-            2
-            <OpenInNewRoundedIcon fontSize="small" sx={{ verticalAlign: "middle", ml: 0.5 }} />
-          </TileContent>
+          {true ? (
+            <TileContent sx={{ textWrap: "nowrap", color: "warning.main", display: "inline-block", ...underlineSx }}>
+              2
+              <OpenInNewRoundedIcon fontSize="small" sx={{ verticalAlign: "middle", ml: 0.5 }} />
+            </TileContent>
+          ) : (
+            <SkeletonWrapper>
+              <TileContent sx={{ textWrap: "nowrap", color: "warning.main", ...underlineSx }}>載入中</TileContent>
+            </SkeletonWrapper>
+          )}
         </Stack>
       </Box>
     </Box>
