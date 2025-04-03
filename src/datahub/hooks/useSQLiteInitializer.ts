6001060ce368ace690ac0bc1b0657179ef781178
1ureka <summerheadquarters001@gmail.com>
2025-04-03 22:53:54 +0800

    在 useSqliteInitializer 鉤子新增支持動態資料庫選擇，並在初始化後重置查詢

--- src/datahub/hooks/useSQLiteInitializer.ts
+++ src/datahub/hooks/useSQLiteInitializer.ts
@@ -1,23 +1,37 @@
 import { useEffect } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 import { useSQLiteStore } from "./useSQLiteStore";
 import { SQLiteClient } from "../data/SQLiteClient";
 
+import { toKeys } from "@/utils/typedBuiltins";
 import forumDb from "@/assets/db/forum.db?url";
+import { useUrl } from "./url";
+
 const databases = {
   forum: forumDb,
 };
 
+const isDbExists = (dbName: string): dbName is keyof typeof databases => {
+  return toKeys(databases).includes(dbName as keyof typeof databases);
+};
+
 export function useSqliteInitializer() {
+  const { searchParams } = useUrl();
   const setClient = useSQLiteStore((s) => s.setClient);
+  const queryClient = useQueryClient();
 
   useEffect(() => {
-    // TODO: const dbName = searchParams.get("db");
+    let dbName = searchParams.get("db");
+    if (dbName === null || dbName.trim() === "") dbName = "forum";
+    if (!isDbExists(dbName)) throw new Error(`要求資料庫 "${dbName}" 不存在`);
 
     const client = new SQLiteClient({
-      dbPath: databases["forum"],
-      storageKey: `sqlite-db-${"forum"}`,
+      dbPath: databases[dbName],
+      storageKey: `sqlite-db-${dbName}`,
     });
 
     setClient(client);
-  }, [setClient]);
+
+    queryClient.resetQueries({ queryKey: [] });
+  }, [setClient, searchParams, queryClient]);
 }
