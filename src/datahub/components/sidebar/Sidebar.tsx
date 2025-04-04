d3786343e03a805a2bee59d408592d073fbefd04
1ureka <summerheadquarters001@gmail.com>
2025-04-05 06:42:46 +0800

    新增側邊欄擴展功能，包含資料庫選擇下拉選單及更新 hash 的功能

--- src/datahub/components/sidebar/Sidebar.tsx
+++ src/datahub/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Box, ButtonBase, Drawer, IconButton, Stack, Tooltip, Typography } from "@mui/material";
+import { Box, ButtonBase, Drawer, IconButton, MenuItem, Stack, TextField, Tooltip, Typography } from "@mui/material";
 import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
 import SchemaRoundedIcon from "@mui/icons-material/SchemaRounded";
 import ViewListRoundedIcon from "@mui/icons-material/ViewListRounded";
@@ -6,7 +6,9 @@ import TerminalRoundedIcon from "@mui/icons-material/TerminalRounded";
 import PlaylistPlayRoundedIcon from "@mui/icons-material/PlaylistPlayRounded";
 import DataExplorationRoundedIcon from "@mui/icons-material/DataExplorationRounded";
 import FirstPageRoundedIcon from "@mui/icons-material/FirstPageRounded";
+import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
 import { useState } from "react";
+import { useUrl } from "@/datahub/hooks/url";
 
 type NavButtonProps = {
   title: string;
@@ -29,6 +31,7 @@ const NavButton = ({ title, active, onClick, children }: NavButtonProps) => {
           boxShadow: active ? 1 : undefined,
           color: active ? "primary.dark" : undefined,
           "&:hover": { bgcolor: active ? "#fffe" : undefined },
+          transition: "all 0.2s ease-in-out",
         }}
       >
         {children}
@@ -54,6 +57,7 @@ const ExpandedNavButton = ({ title, description, icon, active, onClick }: Expand
         bgcolor: active ? "#fffc" : undefined,
         boxShadow: active ? 1 : undefined,
         "&:hover": { bgcolor: active ? "#fffe" : "#fff1" },
+        transition: "all 0.2s ease-in-out",
       }}
     >
       <Box sx={{ display: "flex", gap: 2, alignItems: "center", p: 2, width: 1 }}>
@@ -74,80 +78,119 @@ const ExpandedNavButton = ({ title, description, icon, active, onClick }: Expand
   );
 };
 
-const ExpandedSidebar = ({ open, onClose }: { open: boolean; onClose: () => void }) => (
-  <Drawer
-    anchor="left"
-    open={open}
-    onClose={onClose}
-    sx={{
-      "& .MuiDrawer-paper": {
-        width: 450,
-        maxWidth: "100vw",
-        bgcolor: "primary.dark",
-        p: 3,
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-      },
-    }}
-  >
-    <Box sx={{ display: "flex", alignItems: "center", gap: 2, mb: 2 }}>
-      <DataExplorationRoundedIcon sx={{ fontSize: "3em", color: "#fffc" }} />
-      <Typography variant="h4" component="h1" sx={{ fontFamily: `"timemachine-wa"`, color: "#fffc" }}>
-        資料樣板
-      </Typography>
-    </Box>
+const ExpandedSidebar = ({ open, onClose }: { open: boolean; onClose: () => void }) => {
+  const { searchParams, updateSearchParams, updateHash, hash } = useUrl();
+  const createHandleDbClick = (dbName: string) => () => {
+    updateSearchParams({ db: dbName });
+  };
+  return (
+    <Drawer
+      anchor="left"
+      open={open}
+      onClose={onClose}
+      sx={{
+        "& .MuiDrawer-paper": {
+          width: 450,
+          maxWidth: "100vw",
+          bgcolor: "primary.dark",
+          p: 3,
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        },
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: 2, mb: 2 }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <DataExplorationRoundedIcon sx={{ fontSize: "3em", color: "#fffc" }} />
+          <Typography variant="h4" component="h1" sx={{ fontFamily: `"timemachine-wa"`, color: "#fffc" }}>
+            資料樣板
+          </Typography>
+        </Box>
 
-    <ExpandedNavButton
-      title="概覽"
-      description="快速掌握資料庫狀態與視覺化圖表"
-      icon={<DashboardRoundedIcon sx={{ color: "primary.dark", fontSize: "3.5rem" }} />}
-      active
-      onClick={onClose}
-    />
+        <TextField
+          className="mode-dark"
+          select
+          label="資料庫"
+          variant="outlined"
+          slotProps={{ select: { IconComponent: ExpandMoreRoundedIcon } }}
+          value={searchParams.get("db") || "forum"}
+        >
+          <MenuItem value="forum" dense>
+            <Typography variant="body2" sx={{ color: "text.primary" }} onClick={createHandleDbClick("forum")}>
+              論壇資料庫
+            </Typography>
+          </MenuItem>
+        </TextField>
+      </Box>
 
-    <ExpandedNavButton
-      title="資料庫結構"
-      description="圖像化呈現資料表間的關聯"
-      icon={<SchemaRoundedIcon sx={{ color: "#fffc", fontSize: "3.5rem" }} />}
-      onClick={onClose}
-    />
+      <ExpandedNavButton
+        title="概覽"
+        description="快速掌握資料庫狀態與視覺化圖表"
+        icon={
+          <DashboardRoundedIcon
+            sx={{ color: hash.getParts().length === 0 ? "primary.dark" : "#fffc", fontSize: "3.5rem" }}
+          />
+        }
+        active={hash.getParts().length === 0}
+        onClick={() => {
+          updateHash("");
+          onClose();
+        }}
+      />
 
-    <ExpandedNavButton
-      title="資料表"
-      description="檢視、編輯與標準化資料表內容"
-      icon={<ViewListRoundedIcon sx={{ color: "#fffc", fontSize: "3.5rem" }} />}
-      onClick={onClose}
-    />
+      <ExpandedNavButton
+        title="資料庫結構"
+        description="圖像化呈現資料表間的關聯"
+        icon={
+          <SchemaRoundedIcon
+            sx={{ color: hash.getParts()[0] === "schema" ? "primary.dark" : "#fffc", fontSize: "3.5rem" }}
+          />
+        }
+        active={hash.getParts()[0] === "schema"}
+        onClick={() => {
+          updateHash("schema");
+          onClose();
+        }}
+      />
 
-    <ExpandedNavButton
-      title="撰寫查詢"
-      description="手動撰寫 SQL 以進行自定義查詢"
-      icon={<TerminalRoundedIcon sx={{ color: "#fffc", fontSize: "3.5rem" }} />}
-      onClick={onClose}
-    />
+      <ExpandedNavButton
+        title="資料表"
+        description="檢視、編輯與標準化資料表內容"
+        icon={<ViewListRoundedIcon sx={{ color: "#fffc", fontSize: "3.5rem" }} />}
+        onClick={onClose}
+      />
 
-    <Box sx={{ flex: 1 }} />
+      <ExpandedNavButton
+        title="撰寫查詢"
+        description="手動撰寫 SQL 以進行自定義查詢"
+        icon={<TerminalRoundedIcon sx={{ color: "#fffc", fontSize: "3.5rem" }} />}
+        onClick={onClose}
+      />
 
-    <ExpandedNavButton
-      title="收起側邊欄"
-      icon={<FirstPageRoundedIcon sx={{ color: "#fffc", fontSize: "2rem" }} />}
-      onClick={onClose}
-    />
-  </Drawer>
-);
+      <Box sx={{ flex: 1 }} />
+
+      <ExpandedNavButton
+        title="收起側邊欄"
+        icon={<FirstPageRoundedIcon sx={{ color: "#fffc", fontSize: "2rem" }} />}
+        onClick={onClose}
+      />
+    </Drawer>
+  );
+};
 
 const Sidebar = () => {
+  const { updateHash, hash } = useUrl();
   const [drawerOpen, setDrawerOpen] = useState(false);
   const toggleDrawer = () => setDrawerOpen(!drawerOpen);
 
   return (
     <Stack sx={{ position: "relative", bgcolor: "primary.dark", p: 2, py: 4, color: "#fffc", height: 1, gap: 2 }}>
-      <NavButton title="概覽" active>
+      <NavButton title="概覽" active={hash.getParts().length === 0} onClick={() => updateHash("")}>
         <DashboardRoundedIcon />
       </NavButton>
 
-      <NavButton title="資料庫結構">
+      <NavButton title="資料庫結構" active={hash.getParts()[0] === "schema"} onClick={() => updateHash("schema")}>
         <SchemaRoundedIcon />
       </NavButton>
 
