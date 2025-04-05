d47e935e58061d76360a70885962405828260478
1ureka <summerheadquarters001@gmail.com>
2025-04-05 21:46:38 +0800

    新增 Flow 組件，整合 useFlowChart 鉤子並加入全螢幕按鈕

--- src/datahub/components/schema/Flow.tsx
+++ src/datahub/components/schema/Flow.tsx
@@ -1,7 +1,19 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import OpenInFullRoundedIcon from "@mui/icons-material/OpenInFullRounded";
+import { useFlowChart } from "@/datahub/hooks/read";
 
 const Flow = () => {
-  return <Box sx={{ flex: 1 }}></Box>;
+  const { nodes, isFetching } = useFlowChart();
+
+  return (
+    <Box sx={{ position: "relative", flex: 1 }}>
+      <Box sx={{ position: "absolute", inset: "auto 0 0 auto", color: "text.secondary" }}>
+        <Button color="inherit" variant="outlined" endIcon={<OpenInFullRoundedIcon />} sx={{ m: 2 }}>
+          全螢幕
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default Flow;
