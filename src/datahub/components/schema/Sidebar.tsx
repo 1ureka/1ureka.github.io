0549258acfc1851e7bce4aec1ea32e9fda80edfe
1ureka <summerheadquarters001@gmail.com>
2025-04-05 18:58:29 +0800

    移除 Sidebar 組件中的高度計算邏輯，固定高度為 600，簡化組件結構

--- src/datahub/components/schema/Sidebar.tsx
+++ src/datahub/components/schema/Sidebar.tsx
@@ -1,29 +1,10 @@
-import { useEffect, useRef, useState } from "react";
-import { Box, CircularProgress } from "@mui/material";
+import { Box } from "@mui/material";
 
 const width = 320;
 
 const Sidebar = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState(-1);
-
-  useEffect(() => {
-    if (ref.current) {
-      const { height } = ref.current.getBoundingClientRect();
-      setHeight(height);
-    }
-  }, []);
-
-  if (height < 0) {
-    return (
-      <Box sx={{ width, display: "grid", placeItems: "center", borderRight: 1, borderColor: "divider" }} ref={ref}>
-        <CircularProgress />
-      </Box>
-    );
-  }
-
   return (
-    <Box sx={{ width, borderRight: 1, borderColor: "divider", height, overflow: "auto" }}>
+    <Box sx={{ width, borderRight: 1, borderColor: "divider", height: 600, overflow: "auto" }}>
       <Box sx={{ height: 2000 }} />
     </Box>
   );
