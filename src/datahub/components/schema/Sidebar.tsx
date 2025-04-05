e49ea6391e3e74db7c53f03c050f835468f40082
1ureka <summerheadquarters001@gmail.com>
2025-04-06 01:07:55 +0800

    調整 Sidebar 組件的高度至 650，以改善顯示效果

--- src/datahub/components/schema/Sidebar.tsx
+++ src/datahub/components/schema/Sidebar.tsx
@@ -4,7 +4,7 @@ const width = 320;
 
 const Sidebar = () => {
   return (
-    <Box sx={{ width, borderRight: 1, borderColor: "divider", height: 600, overflow: "auto" }}>
+    <Box sx={{ width, borderRight: 1, borderColor: "divider", height: 650, overflow: "auto" }}>
       <Box sx={{ height: 2000 }} />
     </Box>
   );
