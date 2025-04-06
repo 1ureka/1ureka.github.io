d725591f27da419309fcfe07f59ec2c4198a76ba
1ureka <summerheadquarters001@gmail.com>
2025-04-07 05:18:34 +0800

    新增刪除貼文的 Popover 確認對話框

--- src/forum/components/postElement/shared/SelfActions.tsx
+++ src/forum/components/postElement/shared/SelfActions.tsx
@@ -1,20 +1,60 @@
 import type { FetchPostByIdResult } from "@/forum/data/post";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Popover, Typography } from "@mui/material";
 import EditNoteRoundedIcon from "@mui/icons-material/EditNoteRounded";
 import DeleteSweepRoundedIcon from "@mui/icons-material/DeleteSweepRounded";
 import { routes } from "@/routes";
+import { useState } from "react";
 
 const SelfActions = ({ post }: { post: FetchPostByIdResult }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
       <Button size="small" startIcon={<EditNoteRoundedIcon />} href={`${routes.forum_edit}?postId=${post.id}`}>
         編輯貼文
       </Button>
-      <Button size="small" color="error" startIcon={<DeleteSweepRoundedIcon />}>
+      <Button size="small" color="error" startIcon={<DeleteSweepRoundedIcon />} onClick={handleOpen}>
         刪除貼文
       </Button>
 
-      {/* TODO: popover */}
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        transformOrigin={{ vertical: "top", horizontal: "center" }}
+        elevation={6}
+      >
+        <Box sx={{ p: 2, maxWidth: 300 }}>
+          <Typography variant="subtitle1" sx={{ mb: 1 }}>
+            確認刪除貼文
+          </Typography>
+          <Typography variant="body2" sx={{ mb: 2, color: "text.secondary" }}>
+            確認刪除貼文後，將無法復原，請謹慎操作。
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
+            <Button variant="outlined" size="small" onClick={handleClose} color="inherit">
+              取消
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              size="small"
+              onClick={handleClose}
+              //   disabled={status === "pending"}
+              disableElevation
+            >
+              確認刪除
+            </Button>
+          </Box>
+        </Box>
+      </Popover>
     </Box>
   );
 };
