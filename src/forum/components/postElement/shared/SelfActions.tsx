465c739af0b2ae3bcd5c4ff199445e277793c9b5
1ureka <summerheadquarters001@gmail.com>
2025-04-07 05:14:22 +0800

    簡化 SelfActions 組件中的按鈕樣式，移除不必要的 outlined 變數

--- src/forum/components/postElement/shared/SelfActions.tsx
+++ src/forum/components/postElement/shared/SelfActions.tsx
@@ -7,15 +7,10 @@ import { routes } from "@/routes";
 const SelfActions = ({ post }: { post: FetchPostByIdResult }) => {
   return (
     <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-      <Button
-        variant="outlined"
-        size="small"
-        startIcon={<EditNoteRoundedIcon />}
-        href={`${routes.forum_edit}?postId=${post.id}`}
-      >
+      <Button size="small" startIcon={<EditNoteRoundedIcon />} href={`${routes.forum_edit}?postId=${post.id}`}>
         編輯貼文
       </Button>
-      <Button variant="outlined" size="small" color="error" startIcon={<DeleteSweepRoundedIcon />}>
+      <Button size="small" color="error" startIcon={<DeleteSweepRoundedIcon />}>
         刪除貼文
       </Button>
 
