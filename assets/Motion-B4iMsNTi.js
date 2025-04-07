5d01a0bb693acbfa4e90dbf289f716936326929a
1ureka <summerheadquarters001@gmail.com>
2025-04-07 09:57:51 +0800

    完成 post 的 CRUD

--- assets/Motion-B4iMsNTi.js
+++
@@ -1,1 +0,0 @@
-import{m as o}from"./proxy-ByN5GVJ7.js";import{B as m}from"./routes-CadzfG2E.js";const a=o.create(m);export{a as B};
---
+++ assets/Motion-DjGstyPz.js
@@ -0,0 +1,1 @@
+import{m as o}from"./proxy--ZqoHVXE.js";import{B as m}from"./routes-BtBCtlTZ.js";const a=o.create(m);export{a as B};
