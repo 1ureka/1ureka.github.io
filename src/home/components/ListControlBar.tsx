2637b6c5ceb83245b7f56b4e918c34a15c930d72
1ureka <summerheadquarters001@gmail.com>
2025-04-19 21:13:45 +0800

    新增 SearchBar 組件，提供搜尋功能並整合 URL 參數管理

--- src/home/components/ListControlBar.tsx
+++ src/home/components/ListControlBar.tsx
@@ -1,180 +1,30 @@
-import { Box, IconButton, InputAdornment, MenuItem, TextField, useMediaQuery } from "@mui/material";
-import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import SortRoundedIcon from "@mui/icons-material/SortRounded";
-import ExpandMoreRoundedIcon from "@mui/icons-material/ExpandMoreRounded";
+import { Box, useMediaQuery } from "@mui/material";
+import { SearchFilter } from "./SearchFilter";
+import { OrderSelect } from "./OrderSelect";
+import { SearchBar } from "./SearchBar";
 
 const DesktopBar = () => (
   <Box sx={{ display: "flex", alignItems: "center" }}>
-    <TextField
-      select
-      label="主題"
-      variant="outlined"
-      size="small"
-      defaultValue="all"
-      sx={{
-        "& .MuiOutlinedInput-notchedOutline": {
-          borderRadius: 2,
-          borderTopRightRadius: 0,
-          borderBottomRightRadius: 0,
-        },
-      }}
-      slotProps={{ select: { IconComponent: ExpandMoreRoundedIcon } }}
-    >
-      <MenuItem value="all" dense>
-        全部
-      </MenuItem>
-      <MenuItem value="web" dense>
-        社交
-      </MenuItem>
-      <MenuItem value="tool" dense>
-        工具
-      </MenuItem>
-    </TextField>
-
-    <TextField
-      label="搜尋"
-      variant="outlined"
-      size="small"
-      sx={{
-        "& .MuiOutlinedInput-notchedOutline": {
-          borderRadius: 2,
-          borderTopLeftRadius: 0,
-          borderBottomLeftRadius: 0,
-        },
-      }}
-      slotProps={{
-        input: {
-          endAdornment: (
-            <InputAdornment position="end" sx={{ color: "text.secondary" }}>
-              <IconButton edge="end" color="inherit">
-                <SearchRoundedIcon />
-              </IconButton>
-            </InputAdornment>
-          ),
-        },
-      }}
-    />
-
+    <SearchFilter position="left" />
+    <SearchBar position="right" />
     <Box sx={{ flex: 1 }} />
-
-    <TextField
-      select
-      label="排序"
-      variant="outlined"
-      size="small"
-      defaultValue="latest"
-      sx={{ "& .MuiOutlinedInput-notchedOutline": { borderRadius: 2 } }}
-      slotProps={{
-        input: {
-          startAdornment: (
-            <InputAdornment position="start" sx={{ color: "text.secondary" }}>
-              <SortRoundedIcon />
-            </InputAdornment>
-          ),
-        },
-        select: { IconComponent: ExpandMoreRoundedIcon },
-      }}
-    >
-      <MenuItem value="latest" dense>
-        最新
-      </MenuItem>
-      <MenuItem value="title" dense>
-        標題
-      </MenuItem>
-    </TextField>
+    <OrderSelect position="mid" />
   </Box>
 );
 
 const MobileBar = () => (
   <Box sx={{ display: "flex", alignItems: "stretch", flexDirection: "column", gap: 1 }}>
     <Box sx={{ display: "flex", alignItems: "center" }}>
-      <TextField
-        select
-        label="主題"
-        variant="outlined"
-        size="small"
-        defaultValue="all"
-        fullWidth
-        sx={{
-          "& .MuiOutlinedInput-notchedOutline": {
-            borderRadius: 2,
-            borderTopRightRadius: 0,
-            borderBottomRightRadius: 0,
-          },
-        }}
-        slotProps={{ select: { IconComponent: ExpandMoreRoundedIcon } }}
-      >
-        <MenuItem value="all" dense>
-          全部
-        </MenuItem>
-        <MenuItem value="web" dense>
-          社交
-        </MenuItem>
-        <MenuItem value="tool" dense>
-          工具
-        </MenuItem>
-      </TextField>
-
-      <TextField
-        select
-        label="排序"
-        variant="outlined"
-        size="small"
-        defaultValue="latest"
-        fullWidth
-        sx={{
-          "& .MuiOutlinedInput-notchedOutline": {
-            borderRadius: 2,
-            borderTopLeftRadius: 0,
-            borderBottomLeftRadius: 0,
-          },
-        }}
-        slotProps={{
-          input: {
-            startAdornment: (
-              <InputAdornment position="start" sx={{ color: "text.secondary" }}>
-                <SortRoundedIcon />
-              </InputAdornment>
-            ),
-          },
-          select: { IconComponent: ExpandMoreRoundedIcon },
-        }}
-      >
-        <MenuItem value="latest" dense>
-          最新
-        </MenuItem>
-        <MenuItem value="title" dense>
-          標題
-        </MenuItem>
-      </TextField>
+      <SearchFilter position="left" fullWidth />
+      <OrderSelect position="right" fullWidth />
     </Box>
-
-    <TextField
-      label="搜尋"
-      variant="outlined"
-      size="small"
-      sx={{ "& .MuiOutlinedInput-notchedOutline": { borderRadius: 2 } }}
-      slotProps={{
-        input: {
-          endAdornment: (
-            <InputAdornment position="end" sx={{ color: "text.secondary" }}>
-              <IconButton edge="end" color="inherit">
-                <SearchRoundedIcon />
-              </IconButton>
-            </InputAdornment>
-          ),
-        },
-      }}
-    />
+    <SearchBar position="mid" />
   </Box>
 );
 
 export const ListControlBar = () => {
   const isSm = useMediaQuery((theme) => theme.breakpoints.up("sm"));
 
-  if (isSm) {
-    return <DesktopBar />;
-  } else {
-    return <MobileBar />;
-  }
+  if (isSm) return <DesktopBar />;
+  else return <MobileBar />;
 };
---
+++ src/home/components/SearchBar.tsx
@@ -0,0 +1,55 @@
+import { InputAdornment, TextField } from "@mui/material";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+
+import { z } from "zod";
+import { useUrl } from "@/hooks/url";
+
+const maxLength = 50;
+const qSchema = z.string().max(maxLength, { message: `搜尋關鍵字長度不能超過 ${maxLength} 個字元` });
+
+const outlineElementSelector = "& .MuiOutlinedInput-notchedOutline";
+const sxMap = {
+  left: { [outlineElementSelector]: { borderRadius: 2, borderTopRightRadius: 0, borderBottomRightRadius: 0 } },
+  right: { [outlineElementSelector]: { borderRadius: 2, borderTopLeftRadius: 0, borderBottomLeftRadius: 0 } },
+  mid: { [outlineElementSelector]: { borderRadius: 2 } },
+} as const;
+
+const SearchBar = ({ position, fullWidth }: { position: "left" | "right" | "mid"; fullWidth?: boolean }) => {
+  const { searchParams, updateSearchParams } = useUrl();
+  const { data } = qSchema.safeParse(searchParams.get("q"));
+  const q = data ?? "";
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { data, error } = qSchema.safeParse(e.target.value);
+    if (error) return console.error(error.errors[0].message);
+
+    const value = data ?? "";
+    if (q === value) return;
+
+    if (value === "") updateSearchParams({ q: null }, true);
+    else updateSearchParams({ q: value }, true);
+  };
+
+  return (
+    <TextField
+      label="搜尋"
+      variant="outlined"
+      size="small"
+      value={q}
+      onChange={handleChange}
+      sx={sxMap[position]}
+      fullWidth={fullWidth}
+      slotProps={{
+        input: {
+          endAdornment: (
+            <InputAdornment position="end" sx={{ color: "text.secondary" }}>
+              <SearchRoundedIcon />
+            </InputAdornment>
+          ),
+        },
+      }}
+    />
+  );
+};
+
+export { SearchBar };
