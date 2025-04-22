b3ac0a4500b7c2f3abf264cc43671227fee8f122
1ureka <summerheadquarters001@gmail.com>
2025-04-22 23:53:54 +0800

    重構 TableHeader，移除不必要的 Background 組件，簡化結構並整合樣式生成邏輯

--- src/datahub/components/tables/TableHeader.tsx
+++ src/datahub/components/tables/TableHeader.tsx
@@ -2,7 +2,7 @@ import { Box, Checkbox, Typography, TableCell, TableRow, TableSortLabel } from "
 import SortRoundedIcon from "@mui/icons-material/SortRounded";
 
 import { useSort, useTableColumns } from "@/datahub/hooks/table";
-import { smSpace } from "./commonSx";
+import { generateHeadCellSx, smSpace } from "./commonSx";
 import { ellipsisSx } from "@/utils/commonSx";
 
 const captionBgSx = { p: 0.5, borderRadius: 1, bgcolor: "divider" };
@@ -23,24 +23,6 @@ const Captions = ({ isPk, type }: { isPk: boolean; type: string }) => (
   </Box>
 );
 
-// 因為不知為何 TableHead (display: "table-header-group") 無法套用 border-radius
-const Background = () => (
-  <Box
-    component="tr"
-    aria-hidden="true"
-    sx={{
-      position: "absolute",
-      inset: 0,
-      borderRadius: 3,
-      borderBottomRightRadius: "calc(1 * var(--mui-shape-borderRadius))",
-      borderBottomLeftRadius: "calc(1 * var(--mui-shape-borderRadius))",
-      bgcolor: "divider",
-      opacity: 0.8,
-      pointerEvents: "none",
-    }}
-  />
-);
-
 type TableHeaderProps = { columns: Exclude<ReturnType<typeof useTableColumns>["columnsForTable"], null> };
 
 const TableHeader = ({ columns }: TableHeaderProps) => {
@@ -51,50 +33,47 @@ const TableHeader = ({ columns }: TableHeaderProps) => {
   const unchecked = !checked && !indeterminate;
 
   return (
-    <>
-      <Background />
+    <TableRow>
+      <TableCell padding="checkbox" sx={generateHeadCellSx("first")}>
+        <Checkbox
+          color="default"
+          indeterminate={indeterminate}
+          checked={checked}
+          size="small"
+          sx={{
+            color: !unchecked
+              ? "color-mix(in srgb, var(--mui-palette-text-primary) 30%, var(--mui-palette-primary-main) 70%)"
+              : undefined,
+          }}
+        />
+      </TableCell>
 
-      <TableRow>
-        <TableCell padding="checkbox" sx={{ border: "none" }}>
-          <Checkbox
-            color="default"
-            indeterminate={indeterminate}
-            checked={checked}
-            size="small"
-            sx={{
-              color: !unchecked
-                ? "color-mix(in srgb, var(--mui-palette-text-primary) 30%, var(--mui-palette-primary-main) 70%)"
-                : undefined,
-            }}
-          />
-        </TableCell>
+      {columns.map((column, i) => {
+        const { cid, name, type, align } = column;
+        const isPk = column.pk >= 1;
+        const active = orderBy === i;
+        const scale = active ? (order === "desc" ? 1 : -1) : -1;
+        const justifyContent = align === "right" ? "flex-end" : "flex-start";
+        const isLast = i === columns.length - 1;
 
-        {columns.map((column, i) => {
-          const { cid, name, type, align } = column;
-          const isPk = column.pk >= 1;
-          const active = orderBy === i;
-          const scale = active ? (order === "desc" ? 1 : -1) : -1;
-          const justifyContent = align === "right" ? "flex-end" : "flex-start";
-
-          return (
-            <TableCell key={cid} sx={{ border: "none", minWidth: "10rem" }}>
-              <Box sx={{ display: "flex", alignItems: "center", justifyContent }}>
-                <TableSortLabel
-                  active={active}
-                  direction={active ? order : "asc"}
-                  IconComponent={SortRoundedIcon}
-                  slotProps={{ icon: { sx: { transform: `scaleY(${scale})` } } }}
-                  onClick={createToggleHandler(i)}
-                >
-                  <Typography variant="body2">{name}</Typography>
-                </TableSortLabel>
-                <Captions isPk={isPk} type={type} />
-              </Box>
-            </TableCell>
-          );
-        })}
-      </TableRow>
-    </>
+        return (
+          <TableCell key={cid} sx={{ minWidth: "10rem", ...generateHeadCellSx(isLast ? "last" : "middle") }}>
+            <Box sx={{ display: "flex", alignItems: "center", justifyContent }}>
+              <TableSortLabel
+                active={active}
+                direction={active ? order : "asc"}
+                IconComponent={SortRoundedIcon}
+                slotProps={{ icon: { sx: { transform: `scaleY(${scale})` } } }}
+                onClick={createToggleHandler(i)}
+              >
+                <Typography variant="body2">{name}</Typography>
+              </TableSortLabel>
+              <Captions isPk={isPk} type={type} />
+            </Box>
+          </TableCell>
+        );
+      })}
+    </TableRow>
   );
 };
 
