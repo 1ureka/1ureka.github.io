684faabd69c52b29ff8e0a8c000914f971687b56
1ureka <summerheadquarters001@gmail.com>
2025-04-22 23:53:22 +0800

    重構 TableHeader，移除 loading 狀態處理，簡化組件結構，並優化列渲染邏輯

--- src/datahub/components/tables/TableHeader.tsx
+++ src/datahub/components/tables/TableHeader.tsx
@@ -1,33 +1,9 @@
-import { Box, Checkbox, Skeleton, Typography } from "@mui/material";
-import { TableCell, TableHead, TableRow, TableSortLabel } from "@mui/material";
+import { Box, Checkbox, Typography, TableCell, TableRow, TableSortLabel } from "@mui/material";
 import SortRoundedIcon from "@mui/icons-material/SortRounded";
 
 import { useSort, useTableColumns } from "@/datahub/hooks/table";
 import { smSpace } from "./commonSx";
 import { ellipsisSx } from "@/utils/commonSx";
-
-const TableHeaderLoading = () => (
-  <TableRow>
-    <TableCell padding="checkbox" sx={{ border: "none" }}>
-      <Checkbox disabled size="small" />
-    </TableCell>
-
-    <TableCell sx={{ border: "none" }}>
-      <Skeleton variant="rounded" animation="wave">
-        <Typography variant="body2">載入中. . .</Typography>
-      </Skeleton>
-    </TableCell>
-    {[...Array(4)].map((_, i) => (
-      <TableCell key={i} sx={{ border: "none" }}>
-        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <Skeleton variant="rounded" animation="wave">
-            <Typography variant="body2">載入中. . .</Typography>
-          </Skeleton>
-        </Box>
-      </TableCell>
-    ))}
-  </TableRow>
-);
 
 const captionBgSx = { p: 0.5, borderRadius: 1, bgcolor: "divider" };
 const captionSx = { textTransform: "uppercase", lineHeight: 1, color: "text.secondary" };
@@ -65,12 +41,9 @@ const Background = () => (
   />
 );
 
-type TableHeaderProps = {
-  loading: boolean;
-  columns: ReturnType<typeof useTableColumns>["columnsForTable"];
-};
+type TableHeaderProps = { columns: Exclude<ReturnType<typeof useTableColumns>["columnsForTable"], null> };
 
-const TableHeader = ({ loading, columns }: TableHeaderProps) => {
+const TableHeader = ({ columns }: TableHeaderProps) => {
   const { order, orderBy, createToggleHandler } = useSort(columns?.length ?? 0);
 
   const checked = Math.random() > 0.5;
@@ -78,54 +51,50 @@ const TableHeader = ({ loading, columns }: TableHeaderProps) => {
   const unchecked = !checked && !indeterminate;
 
   return (
-    <TableHead sx={{ position: "relative" }}>
+    <>
       <Background />
 
-      {loading || !columns ? (
-        <TableHeaderLoading />
-      ) : (
-        <TableRow>
-          <TableCell padding="checkbox" sx={{ border: "none" }}>
-            <Checkbox
-              color="default"
-              indeterminate={indeterminate}
-              checked={checked}
-              size="small"
-              sx={{
-                color: !unchecked
-                  ? "color-mix(in srgb, var(--mui-palette-text-primary) 30%, var(--mui-palette-primary-main) 70%)"
-                  : undefined,
-              }}
-            />
-          </TableCell>
+      <TableRow>
+        <TableCell padding="checkbox" sx={{ border: "none" }}>
+          <Checkbox
+            color="default"
+            indeterminate={indeterminate}
+            checked={checked}
+            size="small"
+            sx={{
+              color: !unchecked
+                ? "color-mix(in srgb, var(--mui-palette-text-primary) 30%, var(--mui-palette-primary-main) 70%)"
+                : undefined,
+            }}
+          />
+        </TableCell>
 
-          {columns.map((column, i) => {
-            const { cid, name, type, align } = column;
-            const isPk = column.pk >= 1;
-            const active = orderBy === i;
-            const scale = active ? (order === "desc" ? 1 : -1) : -1;
-            const justifyContent = align === "right" ? "flex-end" : "flex-start";
+        {columns.map((column, i) => {
+          const { cid, name, type, align } = column;
+          const isPk = column.pk >= 1;
+          const active = orderBy === i;
+          const scale = active ? (order === "desc" ? 1 : -1) : -1;
+          const justifyContent = align === "right" ? "flex-end" : "flex-start";
 
-            return (
-              <TableCell key={cid} sx={{ border: "none", minWidth: "10rem" }}>
-                <Box sx={{ display: "flex", alignItems: "center", justifyContent }}>
-                  <TableSortLabel
-                    active={active}
-                    direction={active ? order : "asc"}
-                    IconComponent={SortRoundedIcon}
-                    slotProps={{ icon: { sx: { transform: `scaleY(${scale})` } } }}
-                    onClick={createToggleHandler(i)}
-                  >
-                    <Typography variant="body2">{name}</Typography>
-                  </TableSortLabel>
-                  <Captions isPk={isPk} type={type} />
-                </Box>
-              </TableCell>
-            );
-          })}
-        </TableRow>
-      )}
-    </TableHead>
+          return (
+            <TableCell key={cid} sx={{ border: "none", minWidth: "10rem" }}>
+              <Box sx={{ display: "flex", alignItems: "center", justifyContent }}>
+                <TableSortLabel
+                  active={active}
+                  direction={active ? order : "asc"}
+                  IconComponent={SortRoundedIcon}
+                  slotProps={{ icon: { sx: { transform: `scaleY(${scale})` } } }}
+                  onClick={createToggleHandler(i)}
+                >
+                  <Typography variant="body2">{name}</Typography>
+                </TableSortLabel>
+                <Captions isPk={isPk} type={type} />
+              </Box>
+            </TableCell>
+          );
+        })}
+      </TableRow>
+    </>
   );
 };
 
