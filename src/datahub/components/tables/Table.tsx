f765a2f91725f124e4ff58dc3d99a4ca70e3222d
1ureka <summerheadquarters001@gmail.com>
2025-04-22 23:54:18 +0800

    重構 Table 組件，簡化加載邏輯並整合表頭與表格行的渲染，提升可讀性與維護性

--- src/datahub/components/tables/Table.tsx
+++ src/datahub/components/tables/Table.tsx
@@ -1,21 +1,21 @@
-import { Table, TableBody, TableContainer } from "@mui/material";
+import { Table, TableBody, TableContainer, TableHead } from "@mui/material";
 import { lgSpace } from "./commonSx";
 import { TableHeader } from "./TableHeader";
 import { TableRows } from "./TableRows";
 import { useTableColumns } from "@/datahub/hooks/table";
+import { TableHeaderLoading, TableRowsLoading } from "./TableLoading";
 
 const TableComponent = () => {
-  const { isFetching, selectedTable, columnsForTable: columns } = useTableColumns();
+  const { isFetching, selectedTable: table, columnsForTable: columns } = useTableColumns();
+  const loading = isFetching || !table || !columns;
 
   return (
     <TableContainer sx={{ mt: lgSpace }}>
       <Table sx={{ borderSpacing: "0 0.3rem", borderCollapse: "separate" }}>
-        <TableHeader loading={isFetching} columns={columns} />
-        <TableBody>
-          {!isFetching && columns && selectedTable && (
-            <TableRows params={{ table: selectedTable.name }} columns={columns} />
-          )}
-        </TableBody>
+        <TableHead sx={{ position: "relative" }}>
+          {!loading ? <TableHeader columns={columns} /> : <TableHeaderLoading />}
+        </TableHead>
+        <TableBody>{!loading ? <TableRows table={table.name} columns={columns} /> : <TableRowsLoading />}</TableBody>
       </Table>
     </TableContainer>
   );
