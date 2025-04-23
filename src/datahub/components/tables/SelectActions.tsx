1d6ab16e2600b0e59f00dbaa239fa98b8deb0c80
1ureka <summerheadquarters001@gmail.com>
2025-04-23 17:39:54 +0800

    重構 SelectActions 組件，新增 LoadingDisplay 組件以改善加載狀態顯示，並調整選取數量顯示格式

--- src/datahub/components/tables/SelectActions.tsx
+++ src/datahub/components/tables/SelectActions.tsx
@@ -1,6 +1,8 @@
 import { Box, Button, Tooltip, Typography } from "@mui/material";
 import type { ButtonProps } from "@mui/material";
 import { smSpace } from "./commonSx";
+import { useTableRows } from "@/datahub/hooks/tableRows";
+import { useTableColumns } from "@/datahub/hooks/table";
 
 const secondaryButtonSx: (color: string) => ButtonProps["sx"] = (color) => ({
   "--temporary-color": color,
@@ -10,13 +12,39 @@ const secondaryButtonSx: (color: string) => ButtonProps["sx"] = (color) => ({
   px: 1.5,
 });
 
-const SelectActions = () => {
+const LoadingDisplay = () => (
+  <>
+    <Typography sx={{ color: "text.secondary" }}>已選取 0 個:</Typography>
+
+    <Box sx={{ display: "flex", alignItems: "center", gap: smSpace }}>
+      <Button sx={secondaryButtonSx("var(--mui-palette-primary-main)")} loading>
+        覆蓋欄位
+      </Button>
+      <Button sx={secondaryButtonSx("var(--mui-palette-error-main)")} loading>
+        刪除紀錄
+      </Button>
+    </Box>
+  </>
+);
+
+const SelectActions = (params: Parameters<typeof useTableRows>[0]) => {
+  const { isFetching, data } = useTableRows(params);
+
+  if (isFetching || !data) {
+    return <LoadingDisplay />;
+  }
+
+  const { totalRows } = data;
+  const selectedAmount = 0;
+
   return (
     <>
-      <Typography sx={{ color: "text.secondary" }}>已選取 0 個:</Typography>
+      <Typography sx={{ color: "text.secondary" }}>
+        已選取 {selectedAmount} 個 / {totalRows} 個:
+      </Typography>
 
       <Box sx={{ display: "flex", alignItems: "center", gap: smSpace }}>
-        <Tooltip title="以指定值覆蓋選取資料的某個欄位" arrow>
+        <Tooltip title={<Typography variant="body2">以指定值覆蓋選取資料的某個欄位</Typography>} arrow>
           <Button sx={secondaryButtonSx("var(--mui-palette-primary-main)")}>覆蓋欄位</Button>
         </Tooltip>
         <Button sx={secondaryButtonSx("var(--mui-palette-error-main)")}>刪除紀錄</Button>
@@ -25,4 +53,15 @@ const SelectActions = () => {
   );
 };
 
-export { SelectActions };
+const Wrapper = () => {
+  const { isFetching, selectedTable: table, columnsForTable: columns } = useTableColumns();
+  const loading = isFetching || !table || !columns;
+
+  if (loading) {
+    return <LoadingDisplay />;
+  }
+
+  return <SelectActions table={table.name} columns={columns} />;
+};
+
+export { Wrapper as SelectActions };
