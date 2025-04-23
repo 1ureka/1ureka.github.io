d3d9b5684f478f90304892ac92cae14ff78ef51b
1ureka <summerheadquarters001@gmail.com>
2025-04-24 03:38:35 +0800

    新增 PaperM 組件以支持 Paper 元素的動畫效果

--- src/components/Motion.tsx
+++ src/components/Motion.tsx
@@ -1,7 +1,8 @@
 import { motion } from "motion/react";
-import { Box, Stack } from "@mui/material";
+import { Box, Paper, Stack } from "@mui/material";
 
 const BoxM = motion.create(Box);
 const StackM = motion.create(Stack);
+const PaperM = motion.create(Paper);
 
-export { BoxM, StackM };
+export { BoxM, StackM, PaperM };
