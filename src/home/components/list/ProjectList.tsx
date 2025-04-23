524c512b888b7e448d225b72843855439f6a911d
1ureka <summerheadquarters001@gmail.com>
2025-04-24 04:35:36 +0800

    新增 useProjects 鉤子以管理專案資料，並更新 ProjectList 組件以使用該鉤子顯示專案列表

--- src/home/components/list/ProjectList.tsx
+++ src/home/components/list/ProjectList.tsx
@@ -1,112 +1,44 @@
-import { Box } from "@mui/material";
-import ForumRoundedIcon from "@mui/icons-material/ForumRounded";
-import DataExplorationRoundedIcon from "@mui/icons-material/DataExplorationRounded";
-import CameraRoundedIcon from "@mui/icons-material/CameraRounded";
-
+import { Box, type IconProps, Typography } from "@mui/material";
+import WebStoriesRoundedIcon from "@mui/icons-material/WebStoriesRounded";
+import WebAssetRoundedIcon from "@mui/icons-material/WebAssetRounded";
 import { AnimatePresence } from "motion/react";
-import { ProjectCard, type ProjectCardProps } from "./ProjectCard";
+import { BoxM } from "@/components/Motion";
 
-import { sortObjectArray } from "@/utils/array";
-import { routes } from "@/routes";
-import { useUrl } from "@/hooks/url";
-import { useSearch, type Highlight } from "@/hooks/fuse";
-import { useMemo } from "react";
-import { useOrderState, useFilterState, filterSchema } from "@/home/hooks/useControl";
-import { z } from "zod";
+import { ProjectCard } from "./ProjectCard";
+import { useProjects } from "@/home/hooks/useProjects";
 
-type Project = Omit<ProjectCardProps, "title" | "description"> & {
-  title: string;
-  description: string;
-  time: number;
-  type: z.infer<typeof filterSchema>;
+const GroupName = ({ Icon, title }: { Icon: React.FC<{ sx: IconProps["sx"] }>; title: string }) => {
+  return (
+    <BoxM
+      layout
+      sx={{ gridColumn: "1 / -1", display: "flex", alignItems: "center", gap: 2.5, my: 1, mt: 2.5 }}
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -50 }}
+    >
+      <Icon
+        sx={{ fontSize: "3.5rem", bgcolor: "action.active", borderRadius: 1.5, color: "background.default", p: 1 }}
+      />
+      <Typography variant="h4" component="h3" sx={{ fontFamily: "timemachine-wa" }}>
+        {title}
+      </Typography>
+    </BoxM>
+  );
 };
 
-const projectsArray: Project[] = [
-  {
-    title: "資料樣板",
-    description: "探索資料結構、互動體驗與儀表板 UX 的設計樣板，結合可視化與開發工具的一站式後台模組",
-    color: "#66cccc",
-    icon: <DataExplorationRoundedIcon sx={{ fontSize: "4em", color: "#66cccc" }} />,
-    actionHref: routes.datahub_home,
-    progress: 39,
-    time: 1743264000000,
-    type: "desktop",
-  },
-  {
-    title: "論壇樣板",
-    description: "模擬真實社群互動平台，展示從發文、留言到通知的完整 UI/UX 流程與資料驅動的體驗",
-    color: "#ff9d69",
-    icon: <ForumRoundedIcon sx={{ fontSize: "4em", color: "#ff9d69" }} />,
-    actionHref: routes.forum_home,
-    progress: 85,
-    time: 1741968000000,
-    type: "rwd",
-  },
-  {
-    title: "相簿樣板",
-    description: "探索相簿的 UI/UX 設計樣板，嘗試在瀏覽器中實現 windows 的相簿體驗",
-    color: "#d077a1",
-    icon: <CameraRoundedIcon sx={{ fontSize: "4em", color: "#d077a1" }} />,
-    actionHref: routes.photos_home,
-    progress: 2,
-    time: 1744579200000,
-    type: "desktop",
-  },
-];
-
-const projectsArrayForSearch = projectsArray.map((project) => ({
-  title: project.title,
-  description: project.description,
-}));
-
-type SearchResult = (Project & { highlights: Record<"title" | "description", Highlight[]> })[];
-
 const ProjectList = () => {
-  const { searchParams } = useUrl();
-  const search = useSearch(projectsArrayForSearch, ["title", "description"]);
-
-  const { orderState } = useOrderState();
-  const { filter } = useFilterState();
-
-  const result = useMemo<SearchResult>(() => {
-    const q = searchParams.get("q") ?? "";
-    const isSearch = q.trim().length > 0;
-
-    if (isSearch) {
-      const searchResult = search(q);
-      const formatted = searchResult.map((result) => {
-        const project = projectsArray.find((project) => project.title === result.item.title);
-        if (!project) return null;
-        return { ...project, highlights: result.highlights };
-      });
-
-      return formatted.filter((project) => project !== null);
-    }
-
-    return sortObjectArray(projectsArray, (project) => project[orderState.orderBy], orderState.order).map(
-      (project) => ({
-        ...project,
-        highlights: {
-          title: [{ text: project.title, highlight: false }],
-          description: [{ text: project.description, highlight: false }],
-        },
-      })
-    );
-  }, [searchParams, orderState, search]);
-
-  const filtered = useMemo<SearchResult>(() => {
-    if (filter === "all") return result;
-    return result.filter((project) => project.type === filter);
-  }, [result, filter]);
+  const { projects } = useProjects();
 
   return (
     <Box
       sx={{ display: "grid", gridTemplateColumns: { xs: "1fr", sm: "repeat(auto-fill, minmax(500px, 1fr))" }, gap: 2 }}
     >
-      <AnimatePresence initial={false}>
-        {filtered.map(({ time, title, description, highlights, ...project }) => (
+      <AnimatePresence initial={false} mode="popLayout">
+        {projects.length > 0 && <GroupName key="收錄的專案" Icon={WebStoriesRoundedIcon} title="收錄的專案" />}
+        {projects.map(({ time, title, description, highlights, ...project }) => (
           <ProjectCard key={`${time}${title}${description}`} {...project} {...highlights} />
         ))}
+        <GroupName key="收錄的頁面" Icon={WebAssetRoundedIcon} title="收錄的頁面" />
       </AnimatePresence>
     </Box>
   );
---
+++ src/home/hooks/useProjects.ts
@@ -0,0 +1,104 @@
+import ForumRoundedIcon from "@mui/icons-material/ForumRounded";
+import DataExplorationRoundedIcon from "@mui/icons-material/DataExplorationRounded";
+import CameraRoundedIcon from "@mui/icons-material/CameraRounded";
+
+import { z } from "zod";
+import { routes } from "@/routes";
+import { sortObjectArray } from "@/utils/array";
+import type { ProjectCardProps } from "@/home/components/list/ProjectCard";
+import type { IconProps } from "@mui/material";
+
+import { useFilterState, useOrderState, filterSchema } from "@/home/hooks/useControl";
+import { useSearch, type Highlight } from "@/hooks/fuse";
+import { useUrl } from "@/hooks/url";
+import { useMemo } from "react";
+
+type Project = Omit<ProjectCardProps, "title" | "description" | "icon"> & {
+  title: string;
+  description: string;
+  icon: React.FC<{ sx: IconProps["sx"] }>;
+  time: number;
+  type: z.infer<typeof filterSchema>;
+};
+
+const projectsArray: Project[] = [
+  {
+    title: "資料樣板",
+    description: "探索資料結構、互動體驗與儀表板 UX 的設計樣板，結合可視化與開發工具的一站式後台模組",
+    color: "#66cccc",
+    icon: DataExplorationRoundedIcon,
+    actionHref: routes.datahub_home,
+    progress: 39,
+    time: 1743264000000,
+    type: "desktop",
+  },
+  {
+    title: "論壇樣板",
+    description: "模擬真實社群互動平台，展示從發文、留言到通知的完整 UI/UX 流程與資料驅動的體驗",
+    color: "#ff9d69",
+    icon: ForumRoundedIcon,
+    actionHref: routes.forum_home,
+    progress: 85,
+    time: 1741968000000,
+    type: "rwd",
+  },
+  {
+    title: "相簿樣板",
+    description: "探索相簿的 UI/UX 設計樣板，嘗試在瀏覽器中實現 windows 的相簿體驗",
+    color: "#d077a1",
+    icon: CameraRoundedIcon,
+    actionHref: routes.photos_home,
+    progress: 2,
+    time: 1744579200000,
+    type: "desktop",
+  },
+];
+
+const projectsArrayForSearch = projectsArray.map((project) => ({
+  title: project.title,
+  description: project.description,
+}));
+
+type SearchResult = (Project & { highlights: Record<"title" | "description", Highlight[]> })[];
+
+const useProjects = () => {
+  const { searchParams } = useUrl();
+  const search = useSearch(projectsArrayForSearch, ["title", "description"]);
+
+  const { orderState } = useOrderState();
+  const result = useMemo<SearchResult>(() => {
+    const q = searchParams.get("q") ?? "";
+    const isSearch = q.trim().length > 0;
+
+    if (isSearch) {
+      const searchResult = search(q);
+      const formatted = searchResult.map((result) => {
+        const project = projectsArray.find((project) => project.title === result.item.title);
+        if (!project) return null;
+        return { ...project, highlights: result.highlights };
+      });
+
+      return formatted.filter((project) => project !== null);
+    }
+
+    return sortObjectArray(projectsArray, (project) => project[orderState.orderBy], orderState.order).map(
+      (project) => ({
+        ...project,
+        highlights: {
+          title: [{ text: project.title, highlight: false }],
+          description: [{ text: project.description, highlight: false }],
+        },
+      })
+    );
+  }, [searchParams, orderState, search]);
+
+  const { filter } = useFilterState();
+  const filtered = useMemo<SearchResult>(() => {
+    if (filter === "all") return result;
+    return result.filter((project) => project.type === filter);
+  }, [result, filter]);
+
+  return { projects: filtered };
+};
+
+export { useProjects, projectsArray };
