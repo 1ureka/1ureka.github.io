657e3afcfd9064225ac90e729680eab562ae8e7b
1ureka <summerheadquarters001@gmail.com>
2025-04-23 17:26:11 +0800

    更新 Pagination 組件，修正資料加載狀態處理，調整每頁顯示行數為 7，增強使用者體驗

--- src/datahub/components/tables/Pagination.tsx
+++ src/datahub/components/tables/Pagination.tsx
@@ -5,21 +5,18 @@ const Pagination = (params: Parameters<typeof useTableRows>[0]) => {
   const { columns } = params;
   const { data, isFetching, page, handlePageChange } = useTableRows(params);
 
-  if (isFetching || !data) {
-    return null;
-  }
-
   return (
     <TablePagination
       colSpan={columns.length + 1} // 補上 checkbox 的 colSpan
-      count={data.totalRows}
-      rowsPerPage={10}
-      rowsPerPageOptions={[10]}
+      count={data?.totalRows ?? 0}
+      rowsPerPage={7}
+      rowsPerPageOptions={[7]}
       page={page}
       showFirstButton
       showLastButton
       slotProps={{ select: { inputProps: { "aria-label": "rows per page" } } }}
       onPageChange={handlePageChange}
+      disabled={isFetching}
       sx={{ border: 0 }}
     />
   );
