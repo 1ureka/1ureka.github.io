69a16951b6516366d2672a12498f4944025ff524
1ureka <summerheadquarters001@gmail.com>
2025-04-26 22:34:49 +0800

    新增 useStateWithPrev 和 useStateWithHistory 鉤子，提供狀態管理的歷史記錄功能

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 const useAnchorEl = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -11,4 +11,45 @@ const useAnchorEl = () => {
   return { anchorEl, handleOpen, handleClose };
 };
 
-export { useAnchorEl };
+type State = string | number | boolean | object | undefined | null;
+
+const useStateWithPrev = <T extends State>(initialValue: T) => {
+  const [value, setValue] = useState<{ current: T; prev: T | null }>({ current: initialValue, prev: null });
+
+  const setState = useCallback((update: T | ((prev: T) => T)) => {
+    setValue((prev) => {
+      const prevValue = prev.current;
+      const newValue = typeof update === "function" ? update(prevValue) : update;
+      return { current: newValue, prev: prevValue };
+    });
+  }, []);
+
+  const state = useMemo(() => value.current, [value]);
+  const prevState = useMemo(() => value.prev, [value]);
+
+  return [state, setState, prevState] as const;
+};
+
+const useStateWithHistory = <T extends State>(initialValue: T, maxLength: number) => {
+  const [value, setValue] = useState<T[]>([initialValue]);
+
+  // 透過反轉陣列順序，來讓整段代碼更不容易出bug, 更好懂, 不須 validate maxLength
+  const setState = useCallback(
+    (update: T | ((prev: T) => T)) => {
+      setValue((prev) => {
+        const prevValue = prev[0];
+        const newValue = typeof update === "function" ? update(prevValue) : update;
+        return [newValue, ...prev].slice(0, maxLength);
+      });
+    },
+    [maxLength]
+  );
+
+  const state = useMemo(() => value[0], [value]);
+  const history = useMemo(() => value.slice(1), [value]);
+  const prevState = useMemo(() => value[1], [value]);
+
+  return [state, setState, history, prevState] as const;
+};
+
+export { useAnchorEl, useStateWithPrev, useStateWithHistory };
