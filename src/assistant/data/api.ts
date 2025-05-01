35fe94672634bac54f31c54104711fd5402b7eba
1ureka <summerheadquarters001@gmail.com>
2025-05-01 09:28:54 +0800

    新增 queryApi 函數，實現 API 查詢功能並處理回應

--- src/assistant/data/api.ts
+++ src/assistant/data/api.ts
@@ -1,4 +1,6 @@
 import { tryCatch } from "@/utils/tryCatch";
+import { useApiUrl } from "../hooks/store";
+import { createDeferred } from "@/utils/async";
 
 interface ApiEndpoint {
   method: string;
@@ -23,4 +25,42 @@ const getApiInfo = async (apiUrl: string): Promise<ApiInfo | null> => {
   return json as ApiInfo;
 };
 
-export { getApiInfo };
+interface QueryOptions {
+  onMessage: (fullResponse: string) => void;
+  onComplete?: (fullResponse: string) => void;
+  onError?: (error: string) => void;
+}
+
+const queryApi = (question: string, options: QueryOptions) => {
+  const { onMessage, onComplete, onError } = options;
+
+  const encodedQuestion = encodeURIComponent(question);
+  const apiUrl = useApiUrl();
+  const url = new URL(`${apiUrl}/query?question=${encodedQuestion}`);
+
+  let fullResponse = "";
+  const deferred = createDeferred<void>();
+  const eventSource = new EventSource(url.toString());
+
+  const close = () => {
+    eventSource.close();
+    deferred.resolve();
+  };
+
+  eventSource.addEventListener("message", (e) => {
+    const { data } = e;
+    fullResponse += data;
+    fullResponse = fullResponse.replace(/\\n/g, "\n");
+    onMessage(fullResponse);
+  });
+
+  eventSource.addEventListener("error", () => {
+    if (fullResponse) onComplete?.(fullResponse);
+    else onError?.("請求失敗，請稍後再試。");
+    close();
+  });
+
+  return { close, promise: deferred.promise };
+};
+
+export { getApiInfo, queryApi };
